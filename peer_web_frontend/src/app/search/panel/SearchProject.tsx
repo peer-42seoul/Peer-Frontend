8133fc4a4c8171394f887b960138c61027ec698e
HiHoi <ghtjd5087@gmail.com>
2023-09-12 17:52:51 +0900

    FIX: delete form

--- peer_web_frontend/src/app/search/panel/SearchProject.tsx
+++ peer_web_frontend/src/app/search/panel/SearchProject.tsx
@@ -1,14 +1,11 @@
 'use client'
 
 import { TextField } from '@mui/material'
-import { Form } from 'react-hook-form'
 
 export default function SearchProject() {
   return (
     <>
-      <Form>
-        <TextField placeholder="Project" />
-      </Form>
+      <TextField placeholder="Project" />
     </>
   )
 }
--- peer_web_frontend/src/app/search/panel/SearchStudy.tsx
+++ peer_web_frontend/src/app/search/panel/SearchStudy.tsx
@@ -1,14 +1,11 @@
 'use client'
 
 import { TextField } from '@mui/material'
-import { Form } from 'react-hook-form'
 
 export default function SearchStudy() {
   return (
     <>
-      <Form>
-        <TextField placeholder="Study" />
-      </Form>
+      <TextField placeholder="Study" />
     </>
   )
 }
