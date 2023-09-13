5ceb9fcf71dcfea3fc2977aba3f79e72264000e4
SaltySalt77 <alexiena@naver.com>
2023-09-13 19:44:30 +0900

    Fix: Removed unused component

--- peer_web_frontend/src/app/profile/MyPage/page.tsx
+++ peer_web_frontend/src/app/profile/MyPage/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import React from 'react'
 import ProfileCard from './panel/ProfileCard'
 import ProfileSection from './panel/ProfileSection'
