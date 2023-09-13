33d5547197f118e5ffbb6d7ed2ef16cccfd74da3
Hyeon Ji Kim <guswly801@gmail.com>
2023-09-13 17:10:32 +0900

    Design: 메인 페이지 UI 요소 추가

--- peer_web_frontend/src/app/panel/NavBar.tsx
+++ peer_web_frontend/src/app/panel/NavBar.tsx
@@ -8,6 +8,7 @@ import {
   Tabs,
   useMediaQuery,
 } from '@mui/material'
+import { useRouter } from 'next/navigation'
 
 import { Dispatch, SetStateAction, useState } from 'react'
 
@@ -16,49 +17,59 @@ interface INavProps {
   setValue: Dispatch<SetStateAction<number>>
 }
 
-const PcNav = ({ value, setValue }: INavProps) => (
-  <Tabs
-    orientation="vertical"
-    value={value}
-    onChange={(event, newValue) => setValue(newValue)}
-    sx={{ borderRight: 1, borderColor: 'divider' }}
-    variant="fullWidth"
-  >
-    <Tab label="홈" />
-    <Tab label="히치하이킹" />
-    <Tab label="쇼케이스" />
-    <Tab label="팀페이지" />
-    <Tab label="내 프로필" />
-  </Tabs>
-)
+const PcNav = ({ value, setValue }: INavProps) => {
+  const router = useRouter();
 
-const MobileNav = ({ value, setValue }: INavProps) => (
-  <Paper
-    sx={{
-      width: '100vw',
-      position: 'fixed',
-      bottom: 0,
-      left: 0,
-      right: 0,
-      overflow: 'hidden',
-    }}
-    elevation={3}
-  >
-    <BottomNavigation
-      showLabels
+  return (
+    <Tabs
+      orientation="vertical"
       value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue)
-      }}
+      onChange={(event, newValue) => setValue(newValue)}
+      sx={{ borderRight: 1, borderColor: 'divider' }}
+      variant="fullWidth"
     >
-      <BottomNavigationAction label="홈" />
-      <BottomNavigationAction label="히치하이킹" />
-      <BottomNavigationAction label="쇼케이스" />
-      <BottomNavigationAction label="팀페이지" />
-      <BottomNavigationAction label="내 프로필" />
-    </BottomNavigation>
-  </Paper>
-)
+      <Tab label="홈" onClick={() => {router.push("/")}}/>
+      <Tab label="히치하이킹" onClick={() => {router.push("/")}}/>
+      <Tab label="쇼케이스" onClick={() => {router.push("/")}}/>
+      <Tab label="팀페이지" onClick={() => {router.push("/")}}/>
+      <Tab label="내 프로필" onClick={() => {router.push("/profile/MyPage")}}/>
+    </Tabs>
+  )
+}
+
+const MobileNav = ({ value, setValue }: INavProps) => {
+  const router = useRouter();
+  
+  return (
+    (
+      <Paper
+        sx={{
+          width: '100vw',
+          position: 'fixed',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          overflow: 'hidden',
+        }}
+        elevation={3}
+      >
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue)
+          }}
+        >
+          <BottomNavigationAction label="홈" onClick={() => {router.push("/")}} />
+          <BottomNavigationAction label="히치하이킹" onClick={() => {router.push("/")}}/>
+          <BottomNavigationAction label="쇼케이스" onClick={() => {router.push("/")}}/>
+          <BottomNavigationAction label="팀페이지" onClick={() => {router.push("/")}}/>
+          <BottomNavigationAction label="내 프로필" onClick={() => {router.push("/profile/MyPage")}}/>
+        </BottomNavigation>
+      </Paper>
+    )
+  )
+}
 
 const NavBar = () => {
   const isPc = useMediaQuery('(min-width:481px)')
