82fa03c608a670fc0423d293f53395a251602f77
HiHoi <ghtjd5087@gmail.com>
2023-10-02 19:09:12 +0900

    Feat: Search API adjust

--- peer_web_frontend/src/app/search/panel/SearchBody.tsx
+++ peer_web_frontend/src/app/search/panel/SearchBody.tsx
@@ -12,14 +12,17 @@ import {
   Stack,
 } from '@mui/material'
 import { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import useSWR from 'swr'
+import { fetchStaticData } from '@/api/fetchers'
 
 interface SearchBodyProps {
   onClose: () => void
 }
 
 enum SearchType {
-  STUDY,
-  PROJECT,
+  STUDY = 'study',
+  PROJECT = 'project',
 }
 
 const StyleSeachPc = {
@@ -34,11 +37,25 @@ const StyleSeachMobile = {
 
 export default function SearchBody({ onClose }: SearchBodyProps) {
   const { isPc } = useMedia()
+  const { control, handleSubmit } = useForm()
   const [type, setType] = useState<SearchType>(SearchType.STUDY)
-  const [searchText, setSearchText] = useState<string>('')
+  const [searchWord, setSearchWord] = useState<string>('')
+  const { data, error, isLoading } = useSWR(
+    searchWord
+      ? `https://27366dd1-6e95-4ec6-90c2-062a85a79dfe.mock.pstmn.io/recruitment?type=${type}&keyword=${searchWord}&category=title&sort=latest&page=1&pagesize=10`
+      : null,
+    fetchStaticData,
+  )
 
   const clickStudy = () => setType(SearchType.STUDY)
   const clickProject = () => setType(SearchType.PROJECT)
+  const onSubmit = (data: any) => {
+    setSearchWord(data.searchWord)
+    console.log(searchWord)
+  }
+
+  if (isLoading) return <Typography>로딩중...</Typography>
+  if (error) return <Typography>에러가 발생했습니다</Typography>
 
   return (
     <>
@@ -65,16 +82,26 @@ export default function SearchBody({ onClose }: SearchBodyProps) {
         </ButtonGroup>
 
         <Stack direction="row" spacing={1} sx={{ mt: 1 }}>
-          <TextField
-            sx={{ width: '100%' }}
-            placeholder={type ? '프로젝트를 찾는 중...' : '스터디를 찾는 중...'}
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-          />
-          <Button>검색</Button>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+              name="searchWord"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <TextField
+                  sx={{ width: '100%' }}
+                  placeholder={
+                    type ? '프로젝트를 찾는 중...' : '스터디를 찾는 중...'
+                  }
+                  {...field}
+                />
+              )}
+            />
+            <Button type="submit">검색</Button>
+          </form>
         </Stack>
-
         <SearchHistory />
+        {data ? <Typography>{data}</Typography> : null}
       </Box>
     </>
   )
