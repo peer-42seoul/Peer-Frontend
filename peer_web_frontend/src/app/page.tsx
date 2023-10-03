cb7f359f19fe5a69ccb83f4aca10351156b45769
jujeon <jwjsjwnsstjd@gmail.com>
2023-10-03 17:46:34 +0900

    Test: For check team list open

--- peer_web_frontend/src/app/page.tsx
+++ peer_web_frontend/src/app/page.tsx
@@ -6,7 +6,8 @@ export type ProjectType = 'studies' | 'projects'
 
 export default async function Home() {
   const data = await fetchServerData(
-    'http://localhost:3001/projects-sort-recent',
+    // 'http://localhost:3001/projects-sort-recent',
+    'https://27366dd1-6e95-4ec6-90c2-062a85a79dfe.mock.pstmn.io/projects-sort-recent',
   )
   return <MainPage initData={data} />
 }
