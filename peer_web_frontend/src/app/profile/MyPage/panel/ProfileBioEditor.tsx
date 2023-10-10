65cf5533e9a17e6430e2d615597f79dd4ed01f3d
SaltySalt77 <alexiena@naver.com>
2023-10-10 15:09:38 +0900

    Feat: Add nickname duplicate check

--- peer_web_frontend/src/app/profile/MyPage/panel/ProfileBioEditor.tsx
+++ peer_web_frontend/src/app/profile/MyPage/panel/ProfileBioEditor.tsx
@@ -1,11 +1,12 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import SettingContainer from './SettingContainer'
-import { Avatar, Grid, Typography } from '@mui/material'
+import { Avatar, Button, Grid, InputAdornment, Typography } from '@mui/material'
 import { IProfileCard } from '@/types/IUserProfile'
 import { useForm, Controller } from 'react-hook-form'
 import CuTextField from '@/components/CuTextField'
 import CuTextFieldLabel from '@/components/CuTextFieldLabel'
+// import axios from 'axios'
 
 interface IFormInput {
   nickname: string
@@ -35,12 +36,46 @@ const ProfileBioEditor = ({
     control,
     formState: { errors },
     // getValues,
+    watch,
   } = useForm<IFormInput>({
     defaultValues: defaultValues,
     mode: 'onChange',
   })
 
+  const nickname = watch('nickname')
+
+  // useEffect(() => {
+  //   if (!isNicknameUnique) return
+  //   setIsNicknameUnique(false)
+  // }, [nickname])
+
+  const NicknameCheckButton = ({
+    nickname,
+    setIsNicknameUnique,
+  }: {
+    nickname: string
+    setIsNicknameUnique: (isNicknameUnique: boolean) => void
+  }) => {
+    const onClick = useCallback(() => {
+      console.log('닉네임 중복확인 api', nickname)
+      setIsNicknameUnique(true)
+      // TODO status code가 200이 아닐 경우 false 처리나 toast 띄우기
+    }, [nickname, setIsNicknameUnique])
+
+    return (
+      <Button
+        variant="contained"
+        disabled={errors.nickname ? true : false}
+        onClick={onClick}
+      >
+        중복 확인
+      </Button>
+    )
+  }
+
   const onSubmit = (data: IFormInput) => {
+    console.log('닉네임 중복확인', isNicknameUnique)
+    if (!isNicknameUnique) return
     console.log('on positive click', data)
   }
 
@@ -75,12 +110,28 @@ const ProfileBioEditor = ({
                   <CuTextField
                     id="nickname"
                     variant="outlined"
-                    field={field}
+                    field={{
+                      ...field,
+                      onChange: (e: any[]) => {
+                        field.onChange(e)
+                        if (isNicknameUnique) setIsNicknameUnique(false)
+                      },
+                    }}
                     fullWidth={true}
                     error={errors.nickname ? true : false}
                     autoComplete="off"
                     placeholder="닉네임은 두 글자 이상이어야 합니다."
                     inputProps={{ minLength: 2, maxLength: 7 }}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <NicknameCheckButton
+                            nickname={nickname}
+                            setIsNicknameUnique={setIsNicknameUnique}
+                          />
+                        </InputAdornment>
+                      ),
+                    }}
                   />
                 )}
                 name="nickname"
