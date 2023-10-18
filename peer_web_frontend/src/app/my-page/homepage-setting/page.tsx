3080eecf356a18a2c1fb0f2ab593653b0677e5ac
SaltySalt77 <alexiena@naver.com>
2023-10-18 15:30:50 +0900

    Feat: Add notification setting

---
+++ peer_web_frontend/src/app/my-page/homepage-setting/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NotifSetting from './panel/NotifSetting'
+
+const HomepageSetting = () => {
+  return (
+    <div>
+      <NotifSetting />
+    </div>
+  )
+}
+
+export default HomepageSetting
