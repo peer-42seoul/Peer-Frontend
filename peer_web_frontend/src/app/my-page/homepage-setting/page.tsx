cfc3c9bdeab554419cd99981ef244816c91dab3b
SaltySalt77 <alexiena@naver.com>
2023-10-18 16:15:42 +0900

    Feat: Add Display setting ui

--- peer_web_frontend/src/app/my-page/homepage-setting/page.tsx
+++ peer_web_frontend/src/app/my-page/homepage-setting/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NotifSetting from './panel/NotifSetting'
+import DisplaySetting from './panel/DisplaySetting'
 
 const HomepageSetting = () => {
   return (
     <div>
       <NotifSetting />
+      <DisplaySetting />
     </div>
   )
 }
---
+++ peer_web_frontend/src/app/my-page/homepage-setting/panel/DisplaySetting.tsx
@@ -0,0 +1,64 @@
+'use client'
+import { Button, Skeleton, Stack, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+// import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+
+const ImageButton = ({
+  imageUrl,
+  alt,
+  text,
+  action,
+}: {
+  imageUrl: string
+  alt: string
+  text: string
+  action: () => void
+}) => {
+  console.log(imageUrl, alt)
+  return (
+    <Button onClick={action}>
+      <Stack>
+        {/* <Image src={imageUrl} alt={alt} width={100} height={100} /> 이미지가 없어서 우선 스켈레톤으로 적용 해두었습니다.*/}
+        <Skeleton variant="rectangular" width={100} height={100} />
+        <Typography>{text}</Typography>
+      </Stack>
+    </Button>
+  )
+}
+
+const DisplaySetting = () => {
+  const [themeType, setThemeType] = useState<'light' | 'dark' | 'system'>(
+    'light',
+  )
+
+  useEffect(() => {
+    console.log('API call!')
+  }, [themeType])
+
+  return (
+    <Box>
+      <Typography>화면 스타일</Typography>
+      <ImageButton
+        imageUrl="https://picsum.photos/200"
+        alt="라이트 모드 이미지"
+        text="라이트 모드"
+        action={() => setThemeType('light')}
+      />
+      <ImageButton
+        imageUrl="https://picsum.photos/200"
+        alt="다크 모드 이미지"
+        text="다크 모드"
+        action={() => setThemeType('dark')}
+      />
+      <ImageButton
+        imageUrl="https://picsum.photos/200"
+        alt="기기 설정 이미지"
+        text="기기 설정"
+        action={() => setThemeType('system')}
+      />
+    </Box>
+  )
+}
+
+export default DisplaySetting
