b02b4b56fbcdf85f33759aa740ffe887ae6b3d7a
SaltySalt77 <alexiena@naver.com>
2023-10-23 21:46:00 +0900

    Design: Update layout

--- peer_web_frontend/src/app/my-page/layout.tsx
+++ peer_web_frontend/src/app/my-page/layout.tsx
@@ -1,31 +1,35 @@
-import { Box, Grid } from '@mui/material'
+import { Container, Grid } from '@mui/material'
 import React from 'react'
 import SubNavBar from './panel/SubNavBar'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Box sx={{ width: '844px' }}>
-      <Grid container columns={7} columnSpacing={12}>
-        <Grid item sm={3}>
+    <Container sx={{ maxWidth: '1980' }}>
+      <Grid
+        container
+        columns={7}
+        columnSpacing={[0, 8]}
+        justifyContent={'space-between'}
+        width={1}
+        maxWidth={1280}
+      >
+        <Grid item xs={0} sm={3} sx={{ maxWidth: '244px' }}>
           <SubNavBar />
         </Grid>
         <Grid
           item
-          xs={12}
+          xs={7}
           sm={4}
+          width={1}
           sx={{
-            display: 'flex',
-            width: '908px',
+            maxWidth: '844px',
             padding: '32px',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            gap: '32px',
           }}
         >
           {children}
         </Grid>
       </Grid>
-    </Box>
+    </Container>
   )
 }
 
--- peer_web_frontend/src/app/panel/MuiThemeProvider.tsx
+++ peer_web_frontend/src/app/panel/MuiThemeProvider.tsx
@@ -11,6 +11,17 @@ export const theme = createTheme({
       xl: 1536,
     },
   },
+  components: {
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          '@media (min-width: 1200px)': {
+            maxWidth: '1280px',
+          },
+        },
+      },
+    },
+  },
 })
 
 const MuiThemeProvider = ({ children }: { children: React.ReactNode }) => {
