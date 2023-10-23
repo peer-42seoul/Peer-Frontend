6aae41a0200c8ae235469047e7e101de895d62a4
SaltySalt77 <alexiena@naver.com>
2023-10-24 00:32:13 +0900

    Refact: Add action prop to customize

--- peer_web_frontend/src/components/BackButton.tsx
+++ peer_web_frontend/src/components/BackButton.tsx
@@ -1,15 +1,21 @@
 'use client'
 
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
-import { Button } from '@mui/material'
+import { Button, SxProps } from '@mui/material'
 import { useRouter } from 'next/navigation'
 
-export default function BackButton() {
+export default function BackButton({
+  action,
+  style,
+}: {
+  action?: () => void
+  style?: SxProps
+}) {
   const router = useRouter()
   return (
     <Button
-      style={{ border: 'none', color: 'white' }}
-      onClick={() => router.back()}
+      sx={style ? style : { border: 'none', color: 'white' }}
+      onClick={action ? action : () => router.back()}
     >
       <ArrowBackIosNewIcon />
     </Button>
