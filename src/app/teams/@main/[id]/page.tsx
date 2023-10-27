5b0a2ede7a09ac46058f23ab550ba3afd5a79697
yoouyeon <jyeon.yoon59@gmail.com>
2023-10-27 16:55:18 +0900

    Design: fix main page layout

--- src/app/teams/@main/[id]/page.tsx
+++ src/app/teams/@main/[id]/page.tsx
@@ -1,17 +1,17 @@
 'use client'
 
-import { Typography } from '@mui/material'
+import { Typography, Stack } from '@mui/material'
 import TeamInfoContainer from './panel/TeamInfoContainer'
 
 const TeamsPage = ({ params }: { params: { id: string } }) => {
   const { id } = params
 
   return (
-    <>
-      {/* 뒤로가기 기능 추가하기 */}
-      <Typography>팀리스트로 돌아가기</Typography>
+    <Stack>
+      {/* 팀 리스트로 가는 기능 추가하기 */}
+      <Typography sx={{ color: '#9B9B9B' }}>팀리스트로 돌아가기</Typography>
       <TeamInfoContainer id={Number(id)} />
-    </>
+    </Stack>
   )
 }
 
