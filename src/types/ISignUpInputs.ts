ff6bf02909cac0cc43958fb24b06e900aab3cfd4
hujeong <hujeong@student.42seoul.kr>
2023-10-27 14:07:24 +0900

    Refact: remove unused interface

--- src/types/ISignUpInputs.ts
+++ src/types/ISignUpInputs.ts
@@ -1,53 +1,7 @@
-import { Control, FieldError } from 'react-hook-form'
-
 export interface ISignUpInputs {
   email: string
   code: string
   password: string
   name: string
   nickName: string
-}
-
-interface ISignUpFieldRules {
-  required?: string
-  pattern?: {
-    value: RegExp
-    message: string
-  }
-  minLength?: {
-    value: number
-    message: string
-  }
-  maxLength?: {
-    value: number
-    message: string
-  }
-  validate?: (value: string) => boolean | string
-}
-
-export interface ISignUpField {
-  label: '이메일' | '인증코드' | '비밀번호' | '닉네임' | '이름'
-  name: 'email' | 'code' | 'password' | 'nickName' | 'name'
-  control: Control<ISignUpInputs, any>
-  error: FieldError | undefined
-  rules?: ISignUpFieldRules
-  placeholder?: string
-  onClick?: () => void
-  onChange?: () => void
-  buttonText?: string
-  inValidInput?: boolean
-  inputProps?: {
-    minLength?: number
-    maxLength?: number
-  }
-  type: 'text' | 'password'
-  isSubmitting?: boolean
-}
-
-export interface ISignUpFields {
-  email: ISignUpField
-  code: ISignUpField
-  password: ISignUpField
-  nickName: ISignUpField
-  name: ISignUpField
 }
