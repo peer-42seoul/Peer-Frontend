1fa053655fbfc6bf6f7eeba7071816c71c445d9b
yoouyeon <jyeon.yoon59@gmail.com>
2023-10-27 17:33:00 +0900

    Design: DnD empty component styling

--- src/app/teams/@main/[id]/page.tsx
+++ src/app/teams/@main/[id]/page.tsx
@@ -8,11 +8,21 @@ const TeamsPage = ({ params }: { params: { id: string } }) => {
   const { id } = params
 
   return (
-    <Stack>
+    <Stack spacing={2}>
       {/* 팀 리스트로 가는 기능 추가하기 */}
       <Typography sx={{ color: '#9B9B9B' }}>팀리스트로 돌아가기</Typography>
       <TeamInfoContainer id={Number(id)} />
-      <Box>Drag And Drop</Box>
+      {/* 임시 컴포넌트 */}
+      <Box
+        sx={{
+          width: 600,
+          height: 300,
+          border: 1.55,
+          padding: 2,
+        }}
+      >
+        Drag And Drop
+      </Box>
       <CuButton
         message={'팀페이지 수정'}
         action={() => {}}
