21262eac93f83409f0340989ed57c31c53c1f7b6
yoouyeon <jyeon.yoon59@gmail.com>
2023-10-27 17:05:55 +0900

    Design: fix main page layout

--- src/app/teams/@main/[id]/page.tsx
+++ src/app/teams/@main/[id]/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { Typography, Stack } from '@mui/material'
+import { Typography, Stack, Box } from '@mui/material'
 import TeamInfoContainer from './panel/TeamInfoContainer'
+import CuButton from '@/components/CuButton'
 
 const TeamsPage = ({ params }: { params: { id: string } }) => {
   const { id } = params
@@ -11,6 +12,14 @@ const TeamsPage = ({ params }: { params: { id: string } }) => {
       {/* 팀 리스트로 가는 기능 추가하기 */}
       <Typography sx={{ color: '#9B9B9B' }}>팀리스트로 돌아가기</Typography>
       <TeamInfoContainer id={Number(id)} />
+      <Box>Drag And Drop</Box>
+      <CuButton
+        message={'팀페이지 수정'}
+        action={() => {}}
+        variant={'contained'}
+        fullWidth={false}
+        disabled
+      />
     </Stack>
   )
 }
