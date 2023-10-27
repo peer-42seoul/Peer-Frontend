d35152e947c04f06ba082786e057bed10a8c26ad
yoouyeon <jyeon.yoon59@gmail.com>
2023-10-27 14:37:05 +0900

    Feat: add back button template

--- src/app/teams/@main/[id]/page.tsx
+++ src/app/teams/@main/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Typography } from '@mui/material'
 import TeamInfoContainer from './panel/TeamInfoContainer'
 
 const TeamsPage = ({ params }: { params: { id: string } }) => {
@@ -7,8 +8,8 @@ const TeamsPage = ({ params }: { params: { id: string } }) => {
 
   return (
     <>
-      {/* <Typography>팀 페이지</Typography> */}
-      {/* <Typography>{id}</Typography> */}
+      {/* 뒤로가기 기능 추가하기 */}
+      <Typography>팀리스트로 돌아가기</Typography>
       <TeamInfoContainer id={Number(id)} />
     </>
   )
