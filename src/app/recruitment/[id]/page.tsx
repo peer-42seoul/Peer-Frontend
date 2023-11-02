43c94c47d4bb9f088f214443d721c1002347e276
HiHoi <ghtjd5087@gmail.com>
2023-11-02 18:14:21 +0900

    WIP

--- src/app/recruitment/[id]/page.tsx
+++ src/app/recruitment/[id]/page.tsx
@@ -12,8 +12,8 @@ import {
   List,
   Container,
 } from '@mui/material'
-import { Tag } from '@/types/IPostDetail'
-import Image from 'next/image'
+import { IPostDetail, Tag } from '@/types/IPostDetail'
+// import Image from 'next/image'
 import React, { useMemo } from 'react'
 import RecruitFormModal from './panel/RecruitFormModal'
 import { useSearchParams } from 'next/navigation'
@@ -21,6 +21,8 @@ import RecruitFormText from './panel/RecruitFormText'
 import useMedia from '@/hook/useMedia'
 import ApplyButton from './panel/ApplyButton'
 import LinkButton from './panel/LinkButton'
+import useSWR from 'swr'
+import { defaultGetFetcher } from '@/api/fetchers'
 
 const RecruitDetailPage = ({ params }: { params: { id: string } }) => {
   const type = useSearchParams().get('type') ?? 'projects'
@@ -29,8 +31,17 @@ const RecruitDetailPage = ({ params }: { params: { id: string } }) => {
   const [role, setRole] = React.useState<string>('')
 
   const { isPc } = useMedia()
-  const { data, isLoading, error } = useSWR<IPostDetail>(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/recruit/${params.id}`, defaultGetFetcher);
-  const { data: userData } = useSWR<{nickname: string; profileImageUrl: string}>(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/profile/other/?userId=${data?.user_id}?infoList=profileImageUrl&infoList=nickname`, defaultGetFetcher);
+  const { data, isLoading, error } = useSWR<IPostDetail>(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/recruit/${params.id}`,
+    defaultGetFetcher,
+  )
+  const { data: userData } = useSWR<{
+    nickname: string
+    profileImageUrl: string
+  }>(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/profile/other/?userId=${data?.user_id}?infoList=profileImageUrl&infoList=nickname`,
+    defaultGetFetcher,
+  )
 
   const total = useMemo(() => {
     if (!data) return 0
@@ -48,7 +59,7 @@ const RecruitDetailPage = ({ params }: { params: { id: string } }) => {
   if (isLoading) return <Typography>로딩중...</Typography>
   if (error) return <Typography>에러 발생</Typography>
   if (!data) return <Typography>데이터가 없습니다</Typography>
-          
+
   return (
     <>
       <RecruitFormModal
@@ -61,12 +72,12 @@ const RecruitDetailPage = ({ params }: { params: { id: string } }) => {
       {isPc ? (
         <Container>
           <Stack direction={'row'} gap={4} marginBottom={6}>
-            <Image
+            {/* <Image
               src={userData?.profileUrl}
               alt="leader_profile"
               width={300}
               height={300}
-            />
+            /> */}
             <Box display="flex" flexDirection="column" gap={2}>
               <Stack gap={2} direction="row">
                 <Typography variant="h5">{data?.title}</Typography>
@@ -145,12 +156,12 @@ const RecruitDetailPage = ({ params }: { params: { id: string } }) => {
             </Typography>
             <Typography>{data?.place}</Typography>
           </Stack>
-          <Image
+          {/* <Image
             src={userData?.profileUrl}
             alt="leader_profile"
             width={300}
             height={300}
-          />
+          /> */}
           <RecruitFormText label="총 인원" content={total?.toString() ?? '0'} />
           <RecruitFormText label="목표 작업기간" content={data?.due} />
           <RecruitFormText label="지역" content={data?.region} />
