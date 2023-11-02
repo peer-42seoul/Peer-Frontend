ccab9c1c58df96f9075342e8388a2b6d067fd239
yoouyeon <jyeon.yoon59@gmail.com>
2023-11-02 16:38:12 +0900

    Feat: add message/list mock api

--- src/app/my-page/message/page.tsx
+++ src/app/my-page/message/page.tsx
@@ -2,7 +2,7 @@
 
 import { AxiosInstance } from 'axios'
 import useAxiosWithAuth from '@/api/config'
-import { getFetcherWithInstance } from '@/api/fetchers'
+import { defaultGetFetcher, getFetcherWithInstance } from '@/api/fetchers'
 import { Box, Button, Container } from '@mui/material'
 import { useRef } from 'react'
 import useSWR from 'swr'
@@ -19,11 +19,16 @@ const MessageMain = () => {
   const MessageBox = useRef<HTMLDivElement | null>(null)
   const { isPc } = useMedia()
   const { isOpen, openModal, closeModal } = useModal()
-  const axiosInstance = useAxiosWithAuth()
+  // const axiosInstance = useAxiosWithAuth()
+  // const { data, error, isLoading } = useSWR<IMessagObject[]>(
+  //   [`${process.env.NEXT_PUBLIC_API_URL}/api/v1/message/list`, axiosInstance],
+  //   ([url, axiosInstance]) =>
+  //     getFetcherWithInstance(url, axiosInstance as AxiosInstance),
+  // )
+  // TODO : PR 전 반드시 지울 것 - mock data 확인용 mock api
   const { data, error, isLoading } = useSWR<IMessagObject[]>(
-    [`${process.env.NEXT_PUBLIC_API_URL}/api/v1/message/list`, axiosInstance],
-    ([url, axiosInstance]) =>
-      getFetcherWithInstance(url, axiosInstance as AxiosInstance),
+    '/api/v1/message/list',
+    defaultGetFetcher,
   )
 
   return (
