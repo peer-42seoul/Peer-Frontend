0e25c3211fbbf5f7296504c011ce765aeed6083a
yoouyeon <jyeon.yoon59@gmail.com>
2023-11-03 10:09:56 +0900

    Feat: link Infinite scroll api

--- src/app/my-page/message/[id]/page.tsx
+++ src/app/my-page/message/[id]/page.tsx
@@ -2,8 +2,8 @@
 
 import axios from 'axios'
 import { useSearchParams } from 'next/navigation'
-import useSWR from 'swr'
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import useSWRMutation from 'swr/mutation'
 import {
   Avatar,
   Box,
@@ -14,7 +14,6 @@ import {
   Typography,
 } from '@mui/material'
 import useInfiniteScroll from '@/hook/useInfiniteScroll'
-import { fetchServerData } from '@/api/fetchers'
 
 interface IMessage {
   userId: number // 이 쪽지의 주인
@@ -22,11 +21,6 @@ interface IMessage {
   content: string
   date: string
   isEnd: boolean
-}
-interface IMessageData {
-  MsgTarget: IUser
-  MsgOwner: IUser
-  Msg: IMessage[]
 }
 
 interface IUser {
@@ -155,12 +149,43 @@ const MessageItem = ({ msg, owner, target }: IMessageItemProps) => {
 }
 
 const MessageChatPage = ({ params }: { params: { id: string } }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
   const searchParams = useSearchParams()
   const [updatedData, setUpdatedData] = useState<IMessage[] | undefined>()
   const [owner, setOwner] = useState<IUser>()
   const [target, setTarget] = useState<IUser>()
+  const [isEnd, setIsEnd] = useState<boolean>(false)
+
+  const fetchMoreData = useCallback(
+    async (url: string) => {
+      try {
+        const response = await axios.post(
+          url,
+          {
+            targetId: searchParams.get('target'),
+            conversationalId: params.id,
+            earlyMsgId: updatedData?.[0]?.msgId,
+          },
+          {
+            headers: { 'Cache-Control': 'no-store' },
+          },
+        )
+        return response.data.MsgList.Msg
+      } catch {
+        // TODO : 에러 구체화
+        alert('쪽지를 불러오는데 실패하였습니다.')
+      }
+    },
+    [searchParams, params, updatedData],
+  )
+
+  const { trigger, data } = useSWRMutation(
+    `/api/v1/message/conversation-list/more`,
+    fetchMoreData,
+  )
 
   useEffect(() => {
+    setIsLoading(true)
     const targetId = searchParams.get('target')
     const conversationalId = params.id
     axios
@@ -183,7 +208,24 @@ const MessageChatPage = ({ params }: { params: { id: string } }) => {
         // TODO : 에러 구체화
         alert('쪽지를 불러오는데 실패하였습니다.')
       })
+      .finally(() => {
+        setIsLoading(false)
+      })
   }, [searchParams, params])
+
+  useEffect(() => {
+    if (!data) return
+    setUpdatedData((prevData: IMessage[] | undefined) => {
+      if (!prevData) return data
+      return [...data, ...prevData]
+    })
+    setIsEnd(data[0].isEnd)
+  }, [data])
+
+  const { ref, spinner } = useInfiniteScroll({
+    trigger, // == mutate
+    isEnd,
+  })
 
   const addNewMessage = (newMessage: IMessage) => {
     if (!updatedData) return
@@ -193,46 +235,15 @@ const MessageChatPage = ({ params }: { params: { id: string } }) => {
     })
   }
 
-  // const fetcherWithParams = async (url: string, params: {}) => {
-  //   const response = await axios.get(url, { params })
-  //   return response.data
-  // }
-  // FIXME: 해당 부분을 나중에 post로 다 바꿔야함
-  // const { data, mutate, isLoading } = useSWR(
-  //   `http://localhost:4000/test_detail_${page}`,
-  //   fetchServerData, // FIXME: 여기의 userid는 내 uid
-  // )
-  // console.log('값 까보ㅈ', updatedData?.MsgList?.Msg)
-  // const { target, spinner } = useInfiniteScroll({
-  //   setPage,
-  //   mutate,
-  //   pageLimit,
-  //   page,
-  // })
+  const bottomRef = useRef<HTMLDivElement>()
 
-  // useEffect(() => {
-  //   setOwner(data?.MsgList.MsgOwner)
-  //   setTarget(data?.MsgList.MsgTarget)
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: 'instant' })
+  }, [updatedData])
 
-  //   setUpdatedData((prevData: any) => {
-  //     return {
-  //       ...prevData,
-  //       MsgList: {
-  //         ...prevData?.MsgList, // 이전 데이터를 그대로 가져오고 새로운 Mag데이터를 추가한다.
-  //         Msg: [
-  //           ...(prevData?.MsgList?.Msg || []),
-  //           ...(data?.MsgList.Msg || []),
-  //         ],
-  //       },
-  //     }
-  //   })
-  // }, [data])
-
-  // if (isLoading) return <Box>쪽지를 불러오는 중입니다...</Box>
-  // if (!data) return <Box>빈 쪽지함 입니다!</Box>
-  // console.log('최초 데이터', data.MsgList.MsgOwner)
-
-  if (!updatedData || !owner || !target) return <Box>빈 쪽지함 입니다!</Box>
+  if (isLoading) return <Typography>로딩중... @_@</Typography>
+  if (!updatedData || !owner || !target)
+    return <Typography>빈 쪽지함 입니다!</Typography>
 
   return (
     <Box sx={{ width: '100%', height: '90vh' }}>
@@ -251,6 +262,15 @@ const MessageChatPage = ({ params }: { params: { id: string } }) => {
         <Typography>{target.userNickname}</Typography>
       </Box>
       <Box sx={{ width: '100%', height: '90%', overflowY: 'auto' }}>
+        <Box
+          sx={{
+            bottom: 0,
+            height: '1vh',
+            backgroundColor: 'primary.main',
+          }}
+          ref={ref}
+        ></Box>
+        {spinner && <CircularProgress />}
         {updatedData.map((msgObj: IMessage) => (
           <MessageItem
             key={msgObj.msgId}
@@ -259,15 +279,7 @@ const MessageChatPage = ({ params }: { params: { id: string } }) => {
             target={target}
           />
         ))}
-        {/* {spinner && <CircularProgress />} */}
-        <Box
-          sx={{
-            bottom: 0,
-            height: '1vh',
-            backgroundColor: 'primary.main',
-          }}
-          // ref={target}
-        />
+        <Box ref={bottomRef}></Box>
       </Box>
       <MessageForm targetId={target.userId} addNewMessage={addNewMessage} />
     </Box>
--- src/hook/useInfiniteScroll.tsx
+++ src/hook/useInfiniteScroll.tsx
@@ -1,32 +1,25 @@
 import { debounce } from 'lodash'
-import { useState, useEffect, useRef, Dispatch, SetStateAction } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 /**
  * 무한 스크롤 훅
- * @param setPage 페이지 번호를 변경하는 함수
- * @param mutate 데이터를 가져오는 함수
- * @param pageLimit 마지막 페이지 번호
- * @param page 현재 페이지 번호
+ * @param trigger 데이터를 가져오는 함수
+ * @param isEnd 더이상 데이터를 가져올 수 없는지 여부
  */
 const useInfiniteScroll = ({
-  setPage,
-  mutate,
-  pageLimit,
-  page,
+  trigger,
+  isEnd,
 }: {
-  setPage: Dispatch<SetStateAction<number>>
-  mutate: any
-  pageLimit: number
-  page: number
+  trigger: () => Promise<any>
+  isEnd: boolean
 }) => {
   const [spinner, setSpinner] = useState(false)
-  const target = useRef(null)
+  const ref = useRef<HTMLDivElement>()
 
   const debouncedFetchData = debounce(async () => {
     // 데이터 업데이트. setSpinner을 언제 true할지 정해야.
     setSpinner(true)
-    setPage(page + 1)
-    await mutate()
+    await trigger()
     setSpinner(false)
   }, 1000)
 
@@ -34,9 +27,10 @@ const useInfiniteScroll = ({
     const observer = new IntersectionObserver(
       (entries) => {
         if (entries[0].isIntersecting) {
-          if (!spinner && page < pageLimit) {
-            // 스피너를 표시하고 페이지 번호를 증가시킨 후 디바운스된 데이터 가져오기 함수 호출
+          if (!spinner && !isEnd) {
+            // 스피너를 표시하고 디바운스된 데이터 가져오기 함수 호출
             // 가능한 페이지 양을 도달했다면 더이상 로딩하지 않는다.
+            // isEnd가 true면 더이상 로딩하지 않는다.
             debouncedFetchData()
           }
         }
@@ -44,19 +38,19 @@ const useInfiniteScroll = ({
       { threshold: 0.7 },
     )
 
-    const currentTarget = target.current
+    const currentRef = ref.current
 
-    if (currentTarget) {
-      observer.observe(currentTarget)
+    if (currentRef) {
+      observer.observe(currentRef)
     }
 
     // 컴포넌트가 언마운트되면 IntersectionObserver 해제
     return () => {
-      if (currentTarget) observer.unobserve(currentTarget)
+      if (currentRef) observer.unobserve(currentRef)
     }
-  }, [target, spinner, debouncedFetchData, page, pageLimit])
+  }, [ref, spinner, debouncedFetchData, isEnd])
 
-  return { target, spinner }
+  return { ref, spinner }
 }
 
 export default useInfiniteScroll
