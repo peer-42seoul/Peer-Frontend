8cb04e357897812c8f5b6a9feaa31d043bf1b4df
yoouyeon <jyeon.yoon59@gmail.com>
2023-11-04 02:04:32 +0900

    Fix: message list infinite request

--- src/app/my-page/message/page.tsx
+++ src/app/my-page/message/page.tsx
@@ -1,30 +1,23 @@
 'use client'
 
-import { AxiosInstance } from 'axios'
-import useAxiosWithAuth from '@/api/config'
-import { getFetcherWithInstance } from '@/api/fetchers'
-import { Box, Container } from '@mui/material'
-// import { useRef } from 'react'
 import useSWR from 'swr'
+import { Box, Container } from '@mui/material'
+import useAxiosWithAuth from '@/api/config'
+import CuButton from '@/components/CuButton'
+import useMedia from '@/hook/useMedia'
 import useModal from '@/hook/useModal'
 import { IMessagObject } from '@/types/IMessageInformation'
-import useMedia from '@/hook/useMedia'
-// import MessageList from './MessageList'
-import CuButton from '@/components/CuButton'
 import MessageContainer from './panel/MessageContainer'
-
-// import useAuthStore from '@/states/useAuthStore'
 
 const MessageMain = () => {
   // NOTE : useRef가 필요한 이유? - 필요없음이 확인되면 지우기
   // const MessageBox = useRef<HTMLDivElement | null>(null)
   const { isPc } = useMedia()
   const { isOpen, openModal, closeModal } = useModal()
-  const axiosInstance = useAxiosWithAuth()
+  const axiosWithAuth = useAxiosWithAuth()
   const { data, error, isLoading } = useSWR<IMessagObject[]>(
-    [`${process.env.NEXT_PUBLIC_API_URL}/api/v1/message/list`, axiosInstance],
-    ([url, axiosInstance]) =>
-      getFetcherWithInstance(url, axiosInstance as AxiosInstance),
+    '/api/v1/message/list',
+    (url: string) => axiosWithAuth.get(url).then((res) => res.data),
   )
 
   return (
