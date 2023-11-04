d0d912801bfb6cec424e1549e1bbae153af3b2ac
yoouyeon <jyeon.yoon59@gmail.com>
2023-11-04 14:27:35 +0900

    Feat: add message global state

--- src/app/my-page/message/page.tsx
+++ src/app/my-page/message/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 
+import { useEffect } from 'react'
 import useSWR from 'swr'
 import { Box, Container } from '@mui/material'
 import useAxiosWithAuth from '@/api/config'
 import CuButton from '@/components/CuButton'
 import useMedia from '@/hook/useMedia'
 import useModal from '@/hook/useModal'
+import useMessageDataState from '@/states/useMessageDataState'
 import { IMessagObject } from '@/types/IMessageInformation'
 import MessageContainer from './panel/MessageContainer'
+import MessageWritingFormModal from './panel/MessageWritingFormModal'
 
 const MessageMain = () => {
   // NOTE : useRef가 필요한 이유? - 필요없음이 확인되면 지우기
@@ -19,6 +22,13 @@ const MessageMain = () => {
     '/api/v1/message/list',
     (url: string) => axiosWithAuth.get(url).then((res) => res.data),
   )
+  const { setMessages, resetMessages } = useMessageDataState()
+
+  useEffect(() => {
+    return () => {
+      resetMessages()
+    }
+  }, [])
 
   return (
     <Container sx={{ height: '90vh' }}>
@@ -31,15 +41,20 @@ const MessageMain = () => {
         />
         {/* <Box sx={{ height: '85vh', overflow: 'auto' }} ref={MessageBox}> */}
         <MessageContainer
-          messages={data || []}
+          originalMessageData={data}
           error={error}
           isLoading={isLoading}
           isPC={isPc}
-          isNewMessageModalOpen={isOpen}
-          newMessageModalClose={closeModal}
         />
         {/* </Box> */}
       </Box>
+      {isOpen && (
+        <MessageWritingFormModal
+          isOpen={isOpen}
+          handleClose={closeModal}
+          setMessageData={setMessages}
+        />
+      )}
     </Container>
   )
 }
