272d91aaabd7cf7dc7ac8c9f4067bd701dd355c5
hujeong <hujeong@student.42seoul.kr>
2023-11-08 10:58:50 +0900

    Design: change password form

--- src/app/my-page/privacy/panel/UserInfoEdit.tsx
+++ src/app/my-page/privacy/panel/UserInfoEdit.tsx
@@ -2,7 +2,7 @@
 
 import { Dispatch, SetStateAction } from 'react'
 import { Controller, SubmitHandler, useForm } from 'react-hook-form'
-import { Button, Typography, Container } from '@mui/material'
+import { Button, Typography, Container, Stack } from '@mui/material'
 import CuTextField from '@/components/CuTextField'
 import useAxiosWithAuth from '@/api/config'
 import IToastProps from '@/types/IToastProps'
@@ -77,82 +77,84 @@ export default function UserInfoEdit({
         {authentication ? <Typography>{authentication}</Typography> : null}
         <Button>인증하기</Button>
         <form onSubmit={handleSubmit(changePassword)}>
-          <Typography>비밀번호</Typography>
-          <Controller
-            name="presentPassword"
-            control={control}
-            defaultValue=""
-            rules={{ required: true }}
-            render={({ field }) => (
-              <CuTextField
-                field={field}
-                type="password"
-                autoComplete="off"
-                error={errors.presentPassword ? true : false}
-                placeholder="현재 비밀번호"
-              />
+          <Stack spacing={1}>
+            <Typography>비밀번호</Typography>
+            <Controller
+              name="presentPassword"
+              control={control}
+              defaultValue=""
+              rules={{ required: true }}
+              render={({ field }) => (
+                <CuTextField
+                  field={field}
+                  type="password"
+                  autoComplete="off"
+                  error={errors.presentPassword ? true : false}
+                  placeholder="현재 비밀번호"
+                />
+              )}
+            />
+            <Controller
+              name="newPassword"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                pattern: {
+                  value:
+                    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*<>])[A-Za-z\d~!@#$%^&*<>]{8,}$/i,
+                  message: '8자 이상의 영문, 숫자, 특수문자 조합이어야 합니다',
+                },
+              }}
+              render={({ field }) => (
+                <CuTextField
+                  field={field}
+                  type="password"
+                  autoComplete="off"
+                  error={errors.newPassword ? true : false}
+                  placeholder="새로운 비밀번호"
+                />
+              )}
+            />
+            <Controller
+              name="confirmPassword"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                pattern: {
+                  value:
+                    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*<>])[A-Za-z\d~!@#$%^&*<>]{8,}$/i,
+                  message: '8자 이상의 영문, 숫자, 특수문자 조합이어야 합니다',
+                },
+                validate: (value) =>
+                  value !== getValues('newPassword') &&
+                  '비밀번호가 일치하지 않습니다',
+              }}
+              render={({ field }) => (
+                <CuTextField
+                  field={field}
+                  type="password"
+                  autoComplete="off"
+                  error={errors.confirmPassword ? true : false}
+                  placeholder="새로운 비밀번호 재입력"
+                />
+              )}
+            />
+            {errors.newPassword ? (
+              <Typography color="error">
+                {errors.newPassword.message
+                  ? errors.newPassword.message
+                  : '비밀번호를 입력해주세요'}
+              </Typography>
+            ) : errors.confirmPassword ? (
+              <Typography color="error">
+                {errors.confirmPassword.message}
+              </Typography>
+            ) : (
+              <Typography>&nbsp;</Typography>
             )}
-          />
-          <Controller
-            name="newPassword"
-            control={control}
-            defaultValue=""
-            rules={{
-              required: true,
-              pattern: {
-                value:
-                  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*<>])[A-Za-z\d~!@#$%^&*<>]{8,}$/i,
-                message: '8자 이상의 영문, 숫자, 특수문자 조합이어야 합니다',
-              },
-            }}
-            render={({ field }) => (
-              <CuTextField
-                field={field}
-                type="password"
-                autoComplete="off"
-                error={errors.newPassword ? true : false}
-                placeholder="새로운 비밀번호"
-              />
-            )}
-          />
-          <Controller
-            name="confirmPassword"
-            control={control}
-            defaultValue=""
-            rules={{
-              required: true,
-              pattern: {
-                value:
-                  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*<>])[A-Za-z\d~!@#$%^&*<>]{8,}$/i,
-                message: '8자 이상의 영문, 숫자, 특수문자 조합이어야 합니다',
-              },
-              validate: (value) =>
-                value !== getValues('newPassword') &&
-                '비밀번호가 일치하지 않습니다',
-            }}
-            render={({ field }) => (
-              <CuTextField
-                field={field}
-                type="password"
-                autoComplete="off"
-                error={errors.confirmPassword ? true : false}
-                placeholder="새로운 비밀번호 재입력"
-              />
-            )}
-          />
-          {errors.newPassword ? (
-            <Typography color="error">
-              {errors.newPassword.message
-                ? errors.newPassword.message
-                : '비밀번호를 입력해주세요'}
-            </Typography>
-          ) : errors.confirmPassword ? (
-            <Typography color="error">
-              {errors.confirmPassword.message}
-            </Typography>
-          ) : (
-            <Typography>&nbsp;</Typography>
-          )}
+          </Stack>
           <Button type="submit">변경하기</Button>
         </form>
       </Container>
