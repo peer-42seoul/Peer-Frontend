432d385905e1b482c10d4f913af7822b4322bfaa
Woorim Kim <wrkimlimi@gmail.com>
2023-11-10 19:10:56 +0900

    fix: change handling refreshToken

--- src/app/login/oauth/page.tsx
+++ src/app/login/oauth/page.tsx
@@ -3,12 +3,12 @@
 import { Typography } from '@mui/material'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
-//import { useCookies } from 'react-cookie'
+import { useCookies } from 'react-cookie'
 import useAuthStore from '@/states/useAuthStore'
 import axios from 'axios'
 
 const OauthLogin = () => {
-  //const [cookies, setCookie] = useCookies(['refreshToken'])
+  const [cookies] = useCookies(['refreshToken'])
   const router = useRouter()
   const searchParams = useSearchParams()
   const { login } = useAuthStore()
@@ -17,7 +17,8 @@ const OauthLogin = () => {
     axios.get('/login/oauth', { withCredentials: true }).then((res) => {
       console.log(res)
     })
-    // const refreshToken = cookies.refreshToken
+    const refreshToken = cookies.refreshToken
+    console.log('refreshToken?', refreshToken)
     // setCookie('refreshToken', refreshToken, { path: '/' })
   }
 
