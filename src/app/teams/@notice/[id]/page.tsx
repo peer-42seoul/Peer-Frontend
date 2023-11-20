90c6cfaf1a5aac42a97d4323002fc6605edd51f7
yoouyeon <jyeon.yoon59@gmail.com>
2023-11-20 17:11:34 +0900

    Feat: Create TeamNotice page template

--- src/app/teams/@notice/[id]/page.tsx
+++ src/app/teams/@notice/[id]/page.tsx
@@ -1,10 +1,29 @@
-import { Typography } from '@mui/material'
+'use client'
+import { Typography, Button, Stack, IconButton } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import useMedia from '@/hook/useMedia'
 
 const TeamNotice = () => {
+  const { isPc } = useMedia()
   return (
-    <>
-      <Typography>공지사항</Typography>
-    </>
+    <Stack width={'100%'}>
+      {isPc ? (
+        <Stack direction={'row'} justifyContent={'flex-end'}>
+          <Button variant="contained" startIcon={<AddIcon />}>
+            새 글쓰기
+          </Button>
+        </Stack>
+      ) : null}
+      <Stack direction={'row'} justifyContent={'space-between'}>
+        <Typography>공지사항</Typography>
+        {isPc ? null : (
+          <IconButton>
+            <AddIcon />
+          </IconButton>
+        )}
+      </Stack>
+      {/* <NoticeList /> */}
+    </Stack>
   )
 }
 
