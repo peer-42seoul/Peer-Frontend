308b3e771a55fa8f79d870f6dd14124b578f5fb4
Hyeon Ji Kim <guswly801@gmail.com>
2023-11-21 15:55:59 +0900

    Feat: withCredentials true

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { fetchServerData } from '@/api/fetchers'
 import MainPage from './panel/MainPage'
 
+//{ withCredentials: true }
 export default async function Home() {
   const data = await fetchServerData(
     `${process.env.NEXT_PUBLIC_API_URL}/api/v1/recruit?type=STUDY&sort=latest&page=1&pageSize=6&keyword=&due=&region1=&region2=&place=&status=&tag=`,
