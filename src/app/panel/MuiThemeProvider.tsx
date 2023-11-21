1239204c799779022a49e75c7e4f7c92ce22a78f
SaltySalt77 <alexiena@naver.com>
2023-11-21 17:14:09 +0900

    Feat: Add explict colors to theme

--- src/app/panel/MuiThemeProvider.tsx
+++ src/app/panel/MuiThemeProvider.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+// import { useDarkMode } from '@/states/useDarkMode'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 
 declare module '@mui/material/styles' {
@@ -21,7 +22,6 @@ declare module '@mui/material/styles' {
     Tag: React.CSSProperties
   }
 
-  // allow configuration using `createTheme`
   interface TypographyVariantsOptions {
     HeadlineEmphasis?: React.CSSProperties
     Headline?: React.CSSProperties
@@ -39,9 +39,77 @@ declare module '@mui/material/styles' {
     Caption?: React.CSSProperties
     Tag?: React.CSSProperties
   }
+
+  interface PaletteColorType {
+    strong?: string
+    normal?: string
+    alternative?: string
+    tinted?: string
+    base?: string
+    assistive?: string
+  }
+
+  interface CommonColors {
+    red: PaletteColorType
+    blue: PaletteColorType
+    purple: PaletteColorType
+    green: PaletteColorType
+    yellow: PaletteColorType
+    pink: PaletteColorType
+  }
+
+  interface CommonColorsOptions {
+    red?: PaletteColorType
+    blue?: PaletteColorType
+    purple?: PaletteColorType
+    green?: PaletteColorType
+    yellow?: PaletteColorType
+    pink?: PaletteColorType
+  }
+
+  interface TypeBackground {
+    primary: string
+    secondary: string
+    tertiary: string
+  }
+
+  interface LineColors {
+    base?: string
+    alternative?: string
+  }
+
+  interface PaletteColors {
+    common: CommonColors
+  }
+
+  interface PaletteColorsOptions {
+    common?: CommonColorsOptions
+  }
+
+  interface Palette {
+    line: LineColors
+  }
+  interface PaletteOptions {
+    line?: LineColors
+  }
+
+  interface TypeText {
+    strong: string
+    normal: string
+    alternative: string
+    assistive: string
+    disable: string
+  }
+
+  // interface TextColorsOptions {
+  //   strong?: string
+  //   normal?: string
+  //   alternative?: string
+  //   assistive?: string
+  //   disable?: string
+  // }
 }
 
-// Update the Typography's variant prop options
 declare module '@mui/material/Typography' {
   interface TypographyPropsVariantOverrides {
     HeadlineEmphasis: true
@@ -62,155 +130,219 @@ declare module '@mui/material/Typography' {
   }
 }
 
-export const theme = createTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 480, // mobile과 pc를 구분하기 위해 테마 설정을 하였습니다.
-      md: 900,
-      lg: 1200,
-      xl: 1536,
+const MuiThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  // const mode = useDarkMode().darkMode
+
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 480, // mobile과 pc를 구분하기 위해 테마 설정을 하였습니다.
+        md: 900,
+        lg: 1200,
+        xl: 1536,
+      },
     },
-  },
-  components: {
-    MuiContainer: {
-      styleOverrides: {
-        root: {
-          '@media (min-width: 1200px)': {
-            maxWidth: '1280px',
+    components: {
+      MuiContainer: {
+        styleOverrides: {
+          root: {
+            '@media (min-width: 1200px)': {
+              maxWidth: '1280px',
+            },
+          },
+        },
+      },
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            minWidth: '40px',
+          },
+        },
+      },
+      MuiTypography: {
+        defaultProps: {
+          variantMapping: {
+            HeadlineEmphasis: 'h1',
+            Headline: 'h1',
+            Title1Emphasis: 'h2',
+            Title1: 'h2',
+            Title2Emphasis: 'h3',
+            Title2: 'h3',
+            Title3Emphasis: 'h4',
+            Title3: 'h4',
+            Body1Emphasis: 'p',
+            Body1: 'p',
+            Body2Emphasis: 'p',
+            Body2: 'p',
+            CaptionEmphasis: 'span',
+            Caption: 'span',
+            Tag: 'span',
           },
         },
       },
     },
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          minWidth: '40px',
-        },
-      },
-    },
-    MuiTypography: {
-      defaultProps: {
-        variantMapping: {
-          HeadlineEmphasis: 'h1',
-          Headline: 'h1',
-          Title1Emphasis: 'h2',
-          Title1: 'h2',
-          Title2Emphasis: 'h3',
-          Title2: 'h3',
-          Title3Emphasis: 'h4',
-          Title3: 'h4',
-          Body1Emphasis: 'p',
-          Body1: 'p',
-          Body2Emphasis: 'p',
-          Body2: 'p',
-          CaptionEmphasis: 'span',
-          Caption: 'span',
-          Tag: 'span',
-        },
-      },
-    },
-  },
-  typography: {
-    fontFamily: 'Pretendard Variable, sans-serif',
-    HeadlineEmphasis: {
-      fontSize: '32px',
-      fontStyle: 'normal',
-      fontWeight: 600,
-      lineHeight: '150%',
-    },
-    Headline: {
-      fontSize: '32px',
-      fontStyle: 'normal',
-      fontWeight: 400,
-      lineHeight: '150%',
-    },
-    Title1Emphasis: {
-      fontSize: '24px',
-      fontStyle: 'normal',
-      fontWeight: 600,
-      lineHeight: '150%',
-    },
-    Title1: {
-      fontSize: '24px',
-      fontStyle: 'normal',
-      fontWeight: 400,
-      lineHeight: '150%',
-    },
-    Title2Emphasis: {
-      fontSize: '20px',
-      fontStyle: 'normal',
-      fontWeight: 600,
-      lineHeight: '150%',
-    },
-    Title2: {
-      fontSize: '20px',
-      fontStyle: 'normal',
-      fontWeight: 400,
-      lineHeight: '150%',
-    },
-    Title3Emphasis: {
-      fontSize: '18px',
-      fontStyle: 'normal',
-      fontWeight: 600,
-      lineHeight: '150%',
-      '@media (max-width: 480px)': {
-        fontSize: '15px',
+    typography: {
+      fontFamily: 'Pretendard Variable, sans-serif',
+      HeadlineEmphasis: {
+        fontSize: '32px',
+        fontStyle: 'normal',
         fontWeight: 600,
+        lineHeight: '150%',
       },
-    } as React.CSSProperties,
-    Title3: {
-      fontSize: '18px',
-      fontStyle: 'normal',
-      fontWeight: 400,
-      lineHeight: '150%',
+      Headline: {
+        fontSize: '32px',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        lineHeight: '150%',
+      },
+      Title1Emphasis: {
+        fontSize: '24px',
+        fontStyle: 'normal',
+        fontWeight: 600,
+        lineHeight: '150%',
+      },
+      Title1: {
+        fontSize: '24px',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        lineHeight: '150%',
+      },
+      Title2Emphasis: {
+        fontSize: '20px',
+        fontStyle: 'normal',
+        fontWeight: 600,
+        lineHeight: '150%',
+      },
+      Title2: {
+        fontSize: '20px',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        lineHeight: '150%',
+      },
+      Title3Emphasis: {
+        fontSize: '18px',
+        fontStyle: 'normal',
+        fontWeight: 600,
+        lineHeight: '150%',
+        '@media (max-width: 480px)': {
+          fontSize: '15px',
+          fontWeight: 600,
+        },
+      } as React.CSSProperties,
+      Title3: {
+        fontSize: '18px',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        lineHeight: '150%',
+      },
+      Body1Emphasis: {
+        fontSize: '15px',
+        fontStyle: 'normal',
+        fontWeight: 600,
+        lineHeight: '150%',
+      },
+      Body1: {
+        fontSize: '15px',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        lineHeight: '150%',
+      },
+      Body2Emphasis: {
+        fontSize: '14px',
+        fontStyle: 'normal',
+        fontWeight: 600,
+        lineHeight: '150%',
+      },
+      Body2: {
+        fontSize: '14px',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        lineHeight: '150%',
+      },
+      CaptionEmphasis: {
+        fontSize: '12px',
+        fontStyle: 'normal',
+        fontWeight: 600,
+        lineHeight: 'normal',
+      },
+      Caption: {
+        fontSize: '12px',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        lineHeight: 'normal',
+      },
+      Tag: {
+        fontSize: '12px',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        lineHeight: '150%',
+      },
     },
-    Body1Emphasis: {
-      fontSize: '15px',
-      fontStyle: 'normal',
-      fontWeight: 600,
-      lineHeight: '150%',
+    palette: {
+      mode: 'dark',
+      common: {
+        red: {
+          strong: '#FF5833',
+          normal: '#FF6D4D',
+          alternative: '#FF7D61',
+          tinted: '#FF6D4D33',
+        },
+        blue: {
+          strong: '#3A5DCF',
+          normal: '#4E6ED4',
+          alternative: '#6681DB',
+          tinted: '#4E6ED433',
+        },
+        purple: {
+          strong: '#6F62FE',
+          normal: '#877CFE',
+          alternative: '#A39BFD',
+          tinted: '#877CFE33',
+        },
+        green: {
+          strong: '#489B08',
+          normal: '#53B309',
+          alternative: '#61CD0E',
+          tinted: '#53B30933',
+        },
+        yellow: {
+          strong: '#FFA805',
+          normal: '#FFB01F',
+          alternative: '#FDBC44',
+          tinted: '#FFB01F33',
+        },
+        pink: {
+          strong: '#C44ECA',
+          normal: '#CB62D0',
+          alternative: '#D581D9',
+          tinted: '#CB62D033',
+        },
+      },
+      background: {
+        primary: '#060623',
+        secondary: '#18182B',
+        tertiary: '#22223A',
+      },
+      line: {
+        base: '#35373E',
+        alternative: '#2C2E35',
+      },
+      text: {
+        strong: '#FFFFFF',
+        normal: '#CCCCCC',
+        alternative: '#AAAAAA',
+        assistive: '#42444C',
+        disable: '#292C32',
+      },
+      primary: {
+        main: '#6F62FE',
+        light: '#A39BFD',
+        dark: '#877CFE',
+      },
     },
-    Body1: {
-      fontSize: '15px',
-      fontStyle: 'normal',
-      fontWeight: 400,
-      lineHeight: '150%',
-    },
-    Body2Emphasis: {
-      fontSize: '14px',
-      fontStyle: 'normal',
-      fontWeight: 600,
-      lineHeight: '150%',
-    },
-    Body2: {
-      fontSize: '14px',
-      fontStyle: 'normal',
-      fontWeight: 400,
-      lineHeight: '150%',
-    },
-    CaptionEmphasis: {
-      fontSize: '12px',
-      fontStyle: 'normal',
-      fontWeight: 600,
-      lineHeight: 'normal',
-    },
-    Caption: {
-      fontSize: '12px',
-      fontStyle: 'normal',
-      fontWeight: 400,
-      lineHeight: 'normal',
-    },
-    Tag: {
-      fontSize: '12px',
-      fontStyle: 'normal',
-      fontWeight: 400,
-      lineHeight: '150%',
-    },
-  },
-})
+  })
 
-const MuiThemeProvider = ({ children }: { children: React.ReactNode }) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>
 }
 
