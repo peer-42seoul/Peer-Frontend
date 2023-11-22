696b6a77b9893cd14dcd22bb58edb646a43e4266
yoouyeon <jyeon.yoon59@gmail.com>
2023-11-22 16:35:51 +0900

    Feat: add submit button

---
+++ src/app/teams/@notice/[id]/notice-edit/panel/NoticeEditButton.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import { Stack, Button } from '@mui/material'
+
+// NOTE : postId가 있으면 수정, 없으면 새로 작성
+const NoticeEditButton = ({
+  teamId,
+  postId,
+}: {
+  teamId: string
+  postId?: string
+}) => {
+  const router = useRouter()
+  const handleGoBack = () => {
+    alert('뒤로가기!')
+    // if (postId) router.push(`/teams/${teamId}/notice/${postId}`)
+    // else router.push(`/teams/${teamId}/notice`)
+  }
+  return (
+    <Stack direction={'row'} justifyContent={'flex-end'}>
+      <Button onClick={handleGoBack} variant={'text'}>
+        취소
+      </Button>
+      <Button type={'submit'} form={'notice-form'} variant={'contained'}>
+        {postId ? '완료' : '등록'}
+      </Button>
+    </Stack>
+  )
+}
+
+export default NoticeEditButton
