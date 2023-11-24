f9a50246564a9c9c3bd58a63e43302d37febd845
HiHoi <hoslim@student.42seoul.kr>
2023-11-24 16:26:29 +0900

    fix: main favorite is ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
-import { fetchServerData } from '@/api/fetchers'
 import MainPage from './panel/MainPage'
+import axios from 'axios'
+import { cookies } from 'next/headers'
 
 export default async function Home() {
+  let data
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/v1/recruit?type=STUDY&sort=latest&page=1&pageSize=6&keyword=&due=&region1=&region2=&place=&status=&tag=`,
+      {
+        withCredentials: true,
+        headers: {
+          'Cache-Control': 'no-store',
+          Authorization: `Bearer ${cookies().get('accessToken')?.value}`,
+        },
+      },
+    )
+    data = response.data
+  } catch (e) {
+    console.error(e)
+  }
 
-  const data = await fetchServerData(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/recruit?type=STUDY&sort=latest&page=1&pageSize=6&keyword=&due=&region1=&region2=&place=&status=&tag=`,
-
-  )
   return <MainPage initData={data} />
 }
