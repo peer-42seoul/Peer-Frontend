96ab3a5bf6e85185b0ab208401d97693d5c8a32a
Hyeon Ji Kim <guswly801@gmail.com>
2023-11-24 18:13:49 +0900

    Fix cookie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import MainPage from './panel/MainPage'
 import axios from 'axios'
 import { cookies } from 'next/headers'
+import { getCookie } from 'cookies-next'
 
 export default async function Home() {
+  const accessToken = getCookie('accessToken', { cookies })
   let data
   try {
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/v1/recruit?type=STUDY&sort=latest&page=1&pageSize=6&keyword=&due=&region1=&region2=&place=&status=&tag=`,
+      `${process.env.NEXT_PUBLIC_API_URL}/api/v1/recruit?type=STUDY&sort=latest&page=1&pageSize=6&keyword=&due=1개월&due=12개월 이상&region1=&region2=&place=&status=&tag=`,
       {
         withCredentials: true,
         headers: {
           'Cache-Control': 'no-store',
-          Authorization: `Bearer ${cookies().get('accessToken')?.value}`,
+          Authorization: `Bearer ${accessToken}`,
         },
       },
     )
