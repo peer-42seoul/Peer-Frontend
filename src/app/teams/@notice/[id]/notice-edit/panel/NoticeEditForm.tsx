ecbd25943fe2ee14581cf3d341baa267ae0bb18f
yoouyeon <jyeon.yoon59@gmail.com>
2023-11-27 17:14:24 +0900

    Feat: api connection in NoticeEditForm

--- src/app/teams/@notice/[id]/notice-edit/panel/NoticeEditForm.tsx
+++ src/app/teams/@notice/[id]/notice-edit/panel/NoticeEditForm.tsx
@@ -1,46 +1,58 @@
 'use client'
 import { FormEvent, useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
 import { Stack, Typography, OutlinedInput } from '@mui/material'
+import axios from 'axios'
 
-const NoticeEditForm = ({ postId }: { teamId: string; postId?: string }) => {
+const NoticeEditForm = ({
+  teamId,
+  postId,
+}: {
+  teamId: string
+  postId?: string
+}) => {
+  const router = useRouter()
   const [previousData, setPreviousData] = useState({
     title: '',
     description: '',
   })
+  const [isLoading, setIsLoading] = useState(false)
   useEffect(() => {
     if (postId) {
-      const dummy = {
-        data: {
-          title: '공지사항 제목이 들어오는 자리입니다.',
-          description:
-            '팀이 진행하고자 하는 스터디 혹은 프로젝트에 대해 설명해 주세요. 팀이 진행하고자 하는 스터디 혹은 프로젝트에 대해 설명해 주세요.팀이 진행하고자 하는 스터디 혹은 프로젝트에 대해 설명해 주세요.팀이 진행하고자 하는 스터디 혹은 프로젝트에 대해 설명해 주세요.',
-          isMine: true,
-        },
-        loading: false,
-        error: null,
-      }
-      const { data, error } = dummy
-      if (error || !data) {
-        alert('데이터를 불러오는 데 실패했습니다.')
-      }
-      setPreviousData({
-        title: data.title,
-        description: data.description,
-      })
+      setIsLoading(true)
+      axios
+        .get(`/api/v1/team/notice/${postId}`)
+        .then((res) => {
+          if (!res || !res.data) throw new Error()
+          setPreviousData({
+            title: res.data.title,
+            description: res.data.description,
+          })
+        })
+        .catch(() => {
+          alert('게시글을 불러오는데 실패했습니다.')
+          router.back()
+        })
+        .finally(() => {
+          setIsLoading(false)
+        })
     }
   }, [postId])
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const formData = new FormData(event.currentTarget)
-    const title = formData.get('title') as string
-    const description = formData.get('description') as string
-    alert(
-      "Title: '" +
-        title +
-        "'\nDescription: '" +
-        description +
-        "'\n\n🐧 : 제출 기능 구현하기",
-    )
+    axios
+      .put(`/api/v1/team/notice/${postId}`, {
+        title: formData.get('title') as string,
+        description: formData.get('description') as string,
+      })
+      .then(() => {
+        alert('공지사항을 수정했습니다.')
+        router.push(`/teams/${teamId}/notice/${postId}`)
+      })
+      .catch(() => {
+        alert('공지사항 수정에 실패했습니다.')
+      })
   }
   return (
     <Stack>
@@ -48,6 +60,7 @@ const NoticeEditForm = ({ postId }: { teamId: string; postId?: string }) => {
         <Stack>
           <Typography>제목</Typography>
           <OutlinedInput
+            disabled={isLoading}
             name={'title'}
             placeholder={'제목을 입력해주세요.'}
             defaultValue={previousData?.title ? previousData.title : ''}
@@ -56,6 +69,7 @@ const NoticeEditForm = ({ postId }: { teamId: string; postId?: string }) => {
         <Stack>
           <Typography>내용</Typography>
           <OutlinedInput
+            disabled={isLoading}
             fullWidth
             name={'description'}
             placeholder={'내용을 입력해주세요.'}
