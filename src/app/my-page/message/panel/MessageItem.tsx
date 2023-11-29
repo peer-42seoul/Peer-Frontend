ad605778ddcdbb34b506940824daee7ed5727c61
yoouyeon <jyeon.yoon59@gmail.com>
2023-11-29 17:41:10 +0900

    Feat: Add SwappableComponent

--- src/app/my-page/message/panel/MessageItem.tsx
+++ src/app/my-page/message/panel/MessageItem.tsx
@@ -3,6 +3,9 @@ import { Checkbox, ListItem, ListItemButton, ListItemIcon } from '@mui/material'
 import { IMessageListData } from '@/types/IMessage'
 import MessageItemBase from './MessageItemBase'
 import CuButton from '@/components/CuButton'
+import SwappableComponent from './SwappableComponent'
+import { AxiosResponse } from 'axios'
+import useAxiosWithAuth from '@/api/config'
 
 interface IPCMessageListItemProps {
   message: IMessageListData
@@ -62,17 +65,38 @@ export const MobileMessageListItem = ({
 }: IMobileMessageListItemProps) => {
   const router = useRouter()
   const { targetId, conversationId } = message
+  const axiosWithAuth = useAxiosWithAuth()
+
+  const deleteOneMessage = () => {
+    alert('삭제')
+    // axiosWithAuth
+    //   .delete('/api/v1/message/delete-message', {
+    //     data: {
+    //       target: targetId,
+    //     },
+    //   })
+    //   .catch(() => {
+    //     // 보완 예정
+    //     alert('쪽지 삭제에 실패했습니다.')
+    //   })
+  }
 
   return (
     <ListItem sx={mobileItemWrapperStyle}>
-      <ListItemButton
-        onClick={() =>
-          router.push(`/my-page/message/${conversationId}?target=${targetId}`)
-        }
-        sx={{ flex: '1 0 100%' }}
+      <SwappableComponent
+        gap={{ x: -30, y: 0 }}
+        offset={{ x: '-13%', y: '0' }}
+        eventHandler={deleteOneMessage}
       >
-        <MessageItemBase message={message} />
-      </ListItemButton>
+        <ListItemButton
+          onClick={() =>
+            router.push(`/my-page/message/${conversationId}?target=${targetId}`)
+          }
+          sx={{ flex: '1 0 100%' }}
+        >
+          <MessageItemBase message={message} />
+        </ListItemButton>
+      </SwappableComponent>
       <CuButton
         variant="contained"
         style={mobileItemDeleteButtonStyle}
