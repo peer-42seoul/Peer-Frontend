a0f9797fcb5c07c127f4661b9b2e4e6acab3ead1
yoouyeon <jyeon.yoon59@gmail.com>
2023-12-05 14:05:30 +0900

    Style: Add document link

--- src/components/CuAvatar.style.ts
+++ src/components/CuAvatar.style.ts
@@ -1,5 +1,7 @@
 import { Theme } from '@mui/material'
 
+// docs: https://mui.com/material-ui/react-avatar/
+
 export const CuAvatar = {
   bgcolor: '#FFFFFF',
   border: (theme: Theme) => `1px solid ${theme.palette.line.alternative}`,
