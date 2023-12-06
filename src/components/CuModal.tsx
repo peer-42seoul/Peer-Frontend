78101aa407c7f3d0a3bb05616765d063ebf8fc3f
yoouyeon <jyeon.yoon59@gmail.com>
2023-12-06 16:44:59 +0900

    Refact: Replace CuCuModal with CuModal

--- src/components/CuModal.tsx
+++ src/components/CuModal.tsx
@@ -1,87 +1,16 @@
-import React from 'react'
-import Modal, { ModalProps } from '@mui/material/Modal'
-import { Box, IconButton, Stack, SxProps, Typography } from '@mui/material'
+import {
+  Box,
+  IconButton,
+  Modal,
+  ModalProps,
+  Stack,
+  Typography,
+} from '@mui/material'
 import useMedia from '@/hook/useMedia'
 import ChevronLeft from '@/icons/ChevronLeft'
 import CloseIcon from '@/icons/CloseIcon'
-import * as style from './CuModal.style'
 import CuButton from './CuButton'
-
-//   여기 있는 state를 쓰면 됩니다.
-//   const [open, setOpen] = useState(false)
-//   const handleOpen = () => setOpen(true) // 다른 버튼이나 요소를 얘를 활용해서 모달 핸들링 가능
-//   const handleClose = () => setOpen(false)
-
-const defaultstyle = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-}
-
-const MobileMessagetstyle = {
-  display: 'flex',
-  width: '100%',
-  height: '100svh',
-  justifyContent: 'center',
-  alignItems: 'center',
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-}
-
-interface ICuModal {
-  open: boolean
-  handleClose: () => void
-  children: React.ReactNode
-  ariaTitle: string
-  ariaDescription: string
-  sx?: SxProps
-  style?: SxProps
-  keepMounted?: boolean
-}
-
-const CuModal = ({
-  open,
-  handleClose,
-  children,
-  ariaTitle,
-  ariaDescription,
-  sx,
-  style = defaultstyle,
-  keepMounted,
-}: ICuModal) => {
-  const { isPc } = useMedia()
-
-  return (
-    <>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby={ariaTitle}
-        aria-describedby={ariaDescription}
-        sx={sx}
-        keepMounted={keepMounted}
-      >
-        <Box sx={style ? style : isPc ? defaultstyle : MobileMessagetstyle}>
-          {children}
-        </Box>
-      </Modal>
-    </>
-  )
-}
-
-export default CuModal
+import * as style from './CuModal.style'
 
 interface IModalButton {
   text: string
@@ -102,7 +31,7 @@ const getModalWrapperStyle = (isPc: boolean, mobileFullSize?: boolean) => {
       : style.mobileWrapper
 }
 
-const CuCuModal = ({
+const CuModal = ({
   title,
   containedButton,
   textButton,
@@ -174,4 +103,4 @@ const CuCuModal = ({
   )
 }
 
-export { CuCuModal }
+export default CuModal
