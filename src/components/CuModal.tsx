5de4b2fc636534b9bc4a9c9706a7f647b1e846fe
yoouyeon <jyeon.yoon59@gmail.com>
2023-12-06 22:14:30 +0900

    Feat: Modify onClose Type

--- src/components/CuModal.tsx
+++ src/components/CuModal.tsx
@@ -34,20 +34,21 @@ interface IModalButton {
  * 기본적으로 MUI Modal에서 쓰이는 props를 모두 사용할 수 있습니다.
  * https://mui.com/material-ui/api/modal/
  * @param open Modal의 열림 여부입니다. (필수)
- * @param onClose Modal의 닫기 함수입니다. (필수)
  * (디자인 적용을 위해 추가된 props)
  * @param title Modal의 제목입니다. (필수)
  * @param containedButton Modal의 주 버튼입니다. (필수)
  * @param textButton Modal의 보조 버튼입니다. (선택)
  * @param mobileFullSize 모바일에서 모달이 전체 화면을 차지하도록 합니다. (기본값: false)
+ * @param onClose Modal의 닫기 함수입니다. (필수)
  * confirm 모달처럼 텍스트만 보여주는 모달이라면 CuTextModal을 사용할 수 있습니다.
  */
 
-export interface ICuModalProps extends ModalProps {
+export interface ICuModalProps extends Omit<ModalProps, 'onClose'> {
   title: string
   containedButton: IModalButton
   textButton?: IModalButton
   mobileFullSize?: boolean // 기본적으로 false입니다.
+  onClose: () => void
 }
 
 const getModalWrapperStyle = (isPc: boolean, mobileFullSize?: boolean) => {
@@ -80,7 +81,7 @@ const CuModal = ({
           spacing={'1rem'}
         >
           {!isPc && mobileFullSize ? (
-            <IconButton onClick={() => onClose} sx={style.headerMobileButton}>
+            <IconButton onClick={onClose} sx={style.headerMobileButton}>
               <ChevronLeft />
             </IconButton>
           ) : (
@@ -93,7 +94,7 @@ const CuModal = ({
           >
             {title}
           </Typography>
-          <IconButton onClick={() => onClose} sx={style.headerCloseButton}>
+          <IconButton onClick={onClose} sx={style.headerCloseButton}>
             <CloseIcon />
           </IconButton>
         </Stack>
