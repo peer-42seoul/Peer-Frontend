4343e5b7f0f757af9ec74d77eca38af5be41aa11
yoouyeon <jyeon.yoon59@gmail.com>
2023-12-06 22:42:03 +0900

    Style: Edit Comment

--- src/components/CuModal.tsx
+++ src/components/CuModal.tsx
@@ -30,19 +30,6 @@ interface IModalButton {
   isLoading?: boolean
 }
 
-/**
- * 기본적으로 MUI Modal에서 쓰이는 props를 모두 사용할 수 있습니다.
- * https://mui.com/material-ui/api/modal/
- * @param open Modal의 열림 여부입니다. (필수)
- * (디자인 적용을 위해 추가된 props)
- * @param title Modal의 제목입니다. (필수)
- * @param containedButton Modal의 주 버튼입니다. (필수)
- * @param textButton Modal의 보조 버튼입니다. (선택)
- * @param mobileFullSize 모바일에서 모달이 전체 화면을 차지하도록 합니다. (기본값: false)
- * @param onClose Modal의 닫기 함수입니다. (필수)
- * confirm 모달처럼 텍스트만 보여주는 모달이라면 CuTextModal을 사용할 수 있습니다.
- */
-
 export interface ICuModalProps extends Omit<ModalProps, 'onClose'> {
   title: string
   containedButton: IModalButton
@@ -58,6 +45,21 @@ const getModalWrapperStyle = (isPc: boolean, mobileFullSize?: boolean) => {
       ? style.mobileFullSizeWrapper
       : style.mobileWrapper
 }
+
+/**
+ * confirm 모달처럼 텍스트만 보여주는 모달이라면 CuTextModal을 사용할 수 있습니다.
+ *
+ * 기본적으로 MUI Modal에서 쓰이는 props를 모두 사용할 수 있습니다.
+ * https://mui.com/material-ui/api/modal/
+ * @param open Modal의 열림 여부입니다. (필수)
+ *
+ * (디자인 적용을 위해 추가된 props)
+ * @param title Modal의 제목입니다. (필수)
+ * @param onClose Modal의 닫기 함수입니다. (필수)
+ * @param containedButton (IModalButton) Modal의 주 버튼입니다. (필수)
+ * @param textButton (IModalButton) Modal의 보조 버튼입니다. (선택)
+ * @param mobileFullSize 모바일에서 모달이 전체 화면을 차지하도록 합니다. (기본값: false)
+ */
 
 const CuModal = ({
   title,
