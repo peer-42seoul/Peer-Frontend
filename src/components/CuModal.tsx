26378e4a6db10a7158fcf5eb90defe4d2c7a5e43
yoouyeon <jyeon.yoon59@gmail.com>
2023-12-06 18:02:20 +0900

    Style: Add parameter description

--- src/components/CuModal.tsx
+++ src/components/CuModal.tsx
@@ -12,11 +12,31 @@ import CloseIcon from '@/icons/CloseIcon'
 import CuButton from './CuButton'
 import * as style from './CuModal.style'
 
+/**
+ * IModalButton
+ * @param text 버튼에 표시될 텍스트입니다.
+ * @param onClick 버튼을 클릭했을 때 실행될 함수입니다.
+ */
+
 interface IModalButton {
   text: string
   onClick: () => void
 }
-interface ICuModalProps extends ModalProps {
+
+/**
+ * 기본적으로 MUI Modal에서 쓰이는 props를 모두 사용할 수 있습니다.
+ * https://mui.com/material-ui/api/modal/
+ * @param open Modal의 열림 여부입니다. (필수)
+ * @param onClose Modal의 닫기 함수입니다. (필수)
+ * (디자인 적용을 위해 추가된 props)
+ * @param title Modal의 제목입니다. (필수)
+ * @param containedButton Modal의 주 버튼입니다. (필수)
+ * @param textButton Modal의 보조 버튼입니다. (선택)
+ * @param mobileFullSize 모바일에서 모달이 전체 화면을 차지하도록 합니다. (기본값: false)
+ * confirm 모달처럼 텍스트만 보여주는 모달이라면 CuTextModal을 사용할 수 있습니다.
+ */
+
+export interface ICuModalProps extends ModalProps {
   title: string
   containedButton: IModalButton
   textButton?: IModalButton
@@ -44,14 +64,7 @@ const CuModal = ({
 }: ICuModalProps) => {
   const { isPc } = useMedia()
   return (
-    <Modal
-      open={open}
-      onClose={onClose}
-      aria-labelledby="modal-title"
-      aria-describedby="modal-description"
-      sx={sx}
-      keepMounted={!!keepMounted}
-    >
+    <Modal open={open} onClose={onClose} sx={sx} keepMounted={!!keepMounted}>
       <Stack spacing={'1.5rem'} sx={getModalWrapperStyle(isPc, mobileFullSize)}>
         <Stack
           direction={'row'}
