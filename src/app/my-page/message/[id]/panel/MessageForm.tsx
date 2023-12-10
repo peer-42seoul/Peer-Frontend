0262e3a6fb36441944f684b11d0a2a6145389d82
yoouyeon <jyeon.yoon59@gmail.com>
2023-12-10 23:43:17 +0900

    Feat: Replace alert with toast

--- src/app/my-page/message/[id]/panel/MessageForm.tsx
+++ src/app/my-page/message/[id]/panel/MessageForm.tsx
@@ -1,9 +1,16 @@
-import { Dispatch, FormEvent, SetStateAction, useState } from 'react'
+import {
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useCallback,
+  useState,
+} from 'react'
 import { isAxiosError } from 'axios'
 import { Stack, TextField, Typography, IconButton, styled } from '@mui/material'
 import useAxiosWithAuth from '@/api/config'
 import CuButton from '@/components/CuButton'
 import CuTextField from '@/components/CuTextField'
+import useToast from '@/hook/useToast'
 import SendIcon from '@/icons/SendIcon'
 import { IMessage, IMessageTargetUser } from '@/types/IMessage'
 import * as style from './MessageForm.style'
@@ -32,11 +39,33 @@ const MessageForm = ({
 }: IMessageFormProps) => {
   const axiosWithAuth = useAxiosWithAuth()
   const [content, setContent] = useState<string>('')
+  const {
+    CuToast,
+    isOpen,
+    openToast,
+    closeToast,
+    toastMessage,
+    setToastMessage,
+  } = useToast()
+
+  const resetToast = useCallback(() => {
+    setToastMessage('')
+    closeToast()
+  }, [closeToast, setToastMessage])
+
+  const setToast = useCallback(
+    (message: string) => {
+      setToastMessage(message)
+      openToast()
+    },
+    [openToast, setToastMessage],
+  )
+
   const messageSubmit = async (e: FormEvent<HTMLFormElement>) => {
     try {
       e.preventDefault()
       if (!content) {
-        alert('내용을 입력하세요.')
+        setToast('빈 메시지는 전송할 수 없습니다.')
         return
       }
       const messageData = {
@@ -50,6 +79,7 @@ const MessageForm = ({
       if (response.status === 201) {
         addNewMessage(response.data)
         setContent('')
+        resetToast()
       }
     } catch (error) {
       if (isAxiosError(error) && error.response?.status === 410) {
@@ -64,67 +94,74 @@ const MessageForm = ({
             }
             return prev
           })
-      }
-      alert('메시지 전송에 실패하였습니다.')
+        setToast('상대가 쪽지를 삭제했습니다.') // NOTE : 안내 문구가 괜찮은지 모르겠음.
+      } else setToast('쪽지 전송에 실패했습니다. 다시 시도해주세요.') // NOTE : 안내 문구가 괜찮은지 모르겠음.
     } finally {
       handleClose && handleClose()
     }
   }
 
   return (
-    <form onSubmit={messageSubmit}>
-      {view === 'PC_VIEW' ? (
-        <Stack
-          direction={'row'}
-          alignItems={'center'}
-          spacing={'1rem'}
-          sx={style.pcMessageForm}
-        >
+    <>
+      <form onSubmit={messageSubmit}>
+        {view === 'PC_VIEW' ? (
           <Stack
             direction={'row'}
-            spacing={0}
-            alignItems={'flex-end'}
-            sx={style.pcTextFieldContainer}
+            alignItems={'center'}
+            spacing={'1rem'}
+            sx={style.pcMessageForm}
           >
-            <BorderlessTextField
-              fullWidth
-              multiline
-              rows={4}
+            <Stack
+              direction={'row'}
+              spacing={0}
+              alignItems={'flex-end'}
+              sx={style.pcTextFieldContainer}
+            >
+              <BorderlessTextField
+                fullWidth
+                multiline
+                rows={4}
+                value={content}
+                placeholder="내용을 입력하세요"
+                onChange={(e) =>
+                  setContent(e.target.value.slice(0, MAX_LENGTH))
+                }
+                disabled={disabled}
+              />
+              <Typography color={'text.assistive'} sx={style.messageLength}>
+                {content.length} / {MAX_LENGTH}
+              </Typography>
+            </Stack>
+            <IconButton type="submit" sx={style.pcSendButton}>
+              <SendIcon />
+            </IconButton>
+          </Stack>
+        ) : (
+          <Stack>
+            <TextField
+              sx={{ width: '100%' }}
               value={content}
               placeholder="내용을 입력하세요"
-              onChange={(e) => setContent(e.target.value.slice(0, MAX_LENGTH))}
-              disabled={disabled}
+              variant="outlined"
+              multiline
+              rows={10}
+              onChange={(e) => setContent(e.target.value)}
             />
-            <Typography color={'text.assistive'} sx={style.messageLength}>
-              {content.length} / {MAX_LENGTH}
-            </Typography>
+            <Stack direction={'row'}>
+              <CuButton
+                variant="contained"
+                action={() => handleClose && handleClose()}
+                message="취소"
+              />
+              <CuButton variant="contained" type="submit" message="보내기" />
+            </Stack>
           </Stack>
-          <IconButton type="submit" sx={style.pcSendButton}>
-            <SendIcon />
-          </IconButton>
-        </Stack>
-      ) : (
-        <Stack>
-          <TextField
-            sx={{ width: '100%' }}
-            value={content}
-            placeholder="내용을 입력하세요"
-            variant="outlined"
-            multiline
-            rows={10}
-            onChange={(e) => setContent(e.target.value)}
-          />
-          <Stack direction={'row'}>
-            <CuButton
-              variant="contained"
-              action={() => handleClose && handleClose()}
-              message="취소"
-            />
-            <CuButton variant="contained" type="submit" message="보내기" />
-          </Stack>
-        </Stack>
-      )}
-    </form>
+        )}
+      </form>
+      <CuToast open={isOpen} onClose={closeToast} severity="error">
+        {toastMessage}
+      </CuToast>
+    </>
   )
 }
 
