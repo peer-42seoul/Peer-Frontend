df52cb12f91487d161df9694b5e5f01ce9d873ac
SaltySalt77 <alexiena@naver.com>
2023-12-13 22:54:55 +0900

    Chore: Add svg to reactcomponent webpack plugin

--- next.config.js
+++ next.config.js
@@ -26,27 +26,14 @@ const nextConfig = withPWA({
       'kr1-api-object-storage.nhncloudservice.com',
     ],
   },
-  webpack: (config) => {
-    // 기존 URL 파일로더 규칙을 가져옵니다.
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.('.svg'),
-    )
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
 
-    // 기존 파일로더에 새로 추가한 `@svgr/webpack` 규칙을 추가합니다.
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        // 후에 설명할 리소스 쿼리입니다.
-        resourceQuery: { not: /components/ },
-      },
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: /components/,
-        use: ['@svgr/webpack'],
-      },
-    )
+    return config
   },
 })
 
