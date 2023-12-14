f50b7e36c4fa3b17d5ac48c2c5f85074c7ab9561
SaltySalt77 <alexiena@naver.com>
2023-12-14 17:45:49 +0900

    Feat: Add Autocomplete

--- src/app/showcase/edit/panel/formPanel/SkillInput.tsx
+++ src/app/showcase/edit/panel/formPanel/SkillInput.tsx
@@ -1,38 +1,85 @@
 import React from 'react'
-import { Stack, InputAdornment } from '@mui/material'
-import { TagIcon } from '../icons'
+import { Stack, InputAdornment, Autocomplete } from '@mui/material'
+import TagIcon from '@/icons/TagIcon'
 import LabelWithIcon from '../LabelWithIcon'
 import CuTextField from '@/components/CuTextField'
 import CuButton from '@/components/CuButton'
 import * as Style from '../ShowcaseEditor.style'
 import TagChip from '@/components/TagChip'
+import { UseFormSetValue, UseFormWatch } from 'react-hook-form'
+import { IShowcaseEditorFields } from '@/types/IShowcaseEdit'
+import { ITag } from '@/types/IPostDetail'
 
-const SkillInput = () => {
+const SkillInput = ({
+  setValue,
+  watch,
+}: {
+  setValue: UseFormSetValue<IShowcaseEditorFields>
+  watch: UseFormWatch<IShowcaseEditorFields>
+}) => {
+  const tags = watch('tags')
+  const setData = (data: string[]) => {
+    setValue('tags', data)
+  }
+
+  const tagList: ITag[] = [
+    { name: 'React', color: '#FF5833' },
+    { name: 'Vue', color: '#FF5833' },
+    { name: 'test', color: '#FF5833' },
+  ]
+
+  const nameList = tagList?.map(({ name }) => name)
+
+  const handleInput = (
+    event: React.SyntheticEvent,
+    value: readonly string[],
+  ) => {
+    setData([...value])
+  }
+
+  /* 태그를 지웁니다 */
+  const handleDelete = (index: number) => {
+    setData(tags.filter((tag, cIndex) => cIndex !== index))
+  }
+
   return (
     <Stack direction={'column'} spacing={'0.5rem'} width={'26rem'}>
       <LabelWithIcon
         svgIcon={<TagIcon sx={Style.IconStyle} />}
         message="기술 스택"
       />
-      <CuTextField
-        placeholder="등록할 기술을 입력하세요."
-        fullWidth
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <CuButton
-                variant="text"
-                style={{
-                  color: 'text.normal',
-                  marginRight: '0.75rem',
-                  padding: '0rem 0.25rem',
-                }}
-                message="등록"
-                TypographyProps={{ variant: 'CaptionEmphasis' }}
-              />
-            </InputAdornment>
-          ),
-        }}
+      {/* <TagAutoComplete datas={skills} setData={setData} tagList={tagList} /> */}
+      <Autocomplete
+        disableClearable
+        multiple
+        options={nameList}
+        onChange={handleInput}
+        value={tags}
+        renderTags={() => <></>}
+        sx={{ height: '2rem', width: '26rem' }}
+        renderInput={(params: any) => (
+          <CuTextField
+            {...params}
+            placeholder="등록할 기술을 입력하세요."
+            fullWidth
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <CuButton
+                    variant="text"
+                    style={{
+                      color: 'text.normal',
+                      marginRight: '0.75rem',
+                      padding: '0rem 0.25rem',
+                    }}
+                    message="등록"
+                    TypographyProps={{ variant: 'CaptionEmphasis' }}
+                  />
+                </InputAdornment>
+              ),
+            }}
+          />
+        )}
       />
       <Stack
         spacing={0.75}
@@ -42,77 +89,20 @@ const SkillInput = () => {
         direction={'row'}
         useFlexGap
       >
-        <TagChip
-          name={'React'}
-          color={'#FF5833'}
-          deleteIcon
-          onDelete={() => {
-            console.log('delete')
-          }}
-        />
-
-        <TagChip
-          name={'React'}
-          color={'#FF5833'}
-          deleteIcon
-          onDelete={() => {
-            console.log('delete')
-          }}
-        />
-
-        <TagChip
-          name={'React'}
-          color={'#FF5833'}
-          deleteIcon
-          onDelete={() => {
-            console.log('delete')
-          }}
-        />
-
-        <TagChip
-          name={'React'}
-          color={'#FF5833'}
-          deleteIcon
-          onDelete={() => {
-            console.log('delete')
-          }}
-        />
-
-        <TagChip
-          name={'React'}
-          color={'#FF5833'}
-          deleteIcon
-          onDelete={() => {
-            console.log('delete')
-          }}
-        />
-
-        <TagChip
-          name={'React'}
-          color={'#FF5833'}
-          deleteIcon
-          onDelete={() => {
-            console.log('delete')
-          }}
-        />
-
-        <TagChip
-          name={'React'}
-          color={'#FF5833'}
-          deleteIcon
-          onDelete={() => {
-            console.log('delete')
-          }}
-        />
-
-        <TagChip
-          name={'React'}
-          color={'#FF5833'}
-          deleteIcon
-          onDelete={() => {
-            console.log('delete')
-          }}
-        />
+        {tags?.map((tag: string, idx: number) => {
+          const selectTag = tagList?.find((item) => item.name === tag)
+          return (
+            <TagChip
+              key={idx}
+              name={selectTag?.name ?? ''}
+              color={selectTag?.color ?? ''}
+              deleteIcon
+              onDelete={() => {
+                handleDelete(idx)
+              }}
+            />
+          )
+        })}
       </Stack>
     </Stack>
   )
