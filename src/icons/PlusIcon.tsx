0aebb63fe4b3a292e2fa56e2cc70ed413011b2ea
SaltySalt77 <alexiena@naver.com>
2023-12-16 01:43:02 +0900

    Design: Add PlusIcon

---
+++ src/icons/PlusIcon.tsx
@@ -0,0 +1,21 @@
+import { createSvgIcon } from '@mui/material'
+
+const PlusIcon = createSvgIcon(
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.25"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M10 4.16406V15.8307" />
+    <path d="M4.16797 10H15.8346" />
+  </svg>,
+  'PlusIcon',
+)
+
+export default PlusIcon
