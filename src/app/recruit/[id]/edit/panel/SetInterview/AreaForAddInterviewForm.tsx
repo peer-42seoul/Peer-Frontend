779f9eac8ded090da7c9a4833cf527268b0c9279
jujeon <jwjsjwnsstjd@gmail.com>
2024-01-02 14:54:30 +0900

    Style : make style const variable

--- src/app/recruit/[id]/edit/panel/SetInterview/AreaForAddInterviewForm.tsx
+++ src/app/recruit/[id]/edit/panel/SetInterview/AreaForAddInterviewForm.tsx
@@ -6,6 +6,32 @@ import { IFormInterview } from '@/types/IPostDetail'
 import AddIcon from '@mui/icons-material/Add'
 import useMedia from '@/hook/useMedia'
 
+const QuestionCard = {
+  border: 1,
+  borderRadius: '0.75rem',
+  boxShadow: 1,
+  backgroundColor: 'background.secondary',
+  padding: '1rem 1.25rem 1rem 1.25rem',
+}
+
+const AddQuestionButtonPc = {
+  width: '6.8rem',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  paddingLeft: '1rem',
+  paddingRight: '1.38rem',
+}
+
+const AddQuestionButtonMobile = {
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  paddingLeft: '1rem',
+  paddingRight: '1.38rem',
+}
+
 const AreaForAddInterviewForm = ({
   question,
   setQuestion,
@@ -91,17 +117,10 @@ const AreaForAddInterviewForm = ({
   }
 
   return (
-    <Stack
-      sx={{
-        border: 1,
-        borderRadius: '0.75rem',
-        boxShadow: 1,
-        backgroundColor: 'background.secondary',
-        padding: '1rem 1.25rem 1rem 1.25rem',
-      }}
-      gap={'1rem'}
-    >
+    <Stack sx={QuestionCard} gap={'1rem'}>
+      {/* 질문 분류 선택 */}
       <SelectInterviewType formType={formType} setFormType={setFormType} />
+      {/* 질문란 작성 */}
       <TextField
         variant="standard"
         value={question}
@@ -109,7 +128,9 @@ const AreaForAddInterviewForm = ({
         placeholder="질문을 입력해주세요."
         sx={{ width: '70%' }}
       />
+      {/* 답변란 작성 */}
       <Stack gap={'0.5rem'}>
+        {/* 질문 분류에 따른 답변 종류 */}
         <WriteAnswers
           answer={answer}
           setAnswer={setAnswer}
@@ -123,26 +144,9 @@ const AreaForAddInterviewForm = ({
           valueOfMax={valueOfMax}
           setvalueOfMax={setvalueOfMax}
         />
+        {/* 질문 추가 버튼 */}
         <Button
-          sx={
-            isPc
-              ? {
-                  width: '6.8rem',
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  paddingLeft: '1rem',
-                  paddingRight: '1.38rem',
-                }
-              : {
-                  width: '100%',
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  paddingLeft: '1rem',
-                  paddingRight: '1.38rem',
-                }
-          }
+          sx={isPc ? AddQuestionButtonPc : AddQuestionButtonMobile}
           variant="outlined"
           onClick={() => {
             onHandlerAddQuestion
