57b2be31865dcc62f4efd2c96e8d6a0f6855ade9
SaltySalt77 <alexiena@naver.com>
2024-01-04 23:05:51 +0900

    Design: Changed dropdown design

--- src/app/hitchhiking/panel/DropdownMenu.tsx
+++ src/app/hitchhiking/panel/DropdownMenu.tsx
@@ -3,6 +3,8 @@ import { Button, Menu, MenuItem, Stack, Typography } from '@mui/material'
 import ShareIcon from '@/icons/ShareIcon'
 import MoreHorizontalIcon from '@/icons/MoreHorizontalIcon'
 import ReportIcon from '@/icons/ReportIcon'
+import useMedia from '@/hook/useMedia'
+import * as style from './DropdownMenu.style'
 
 const IconMenuItem = ({
   handleClose,
@@ -14,8 +16,14 @@ const IconMenuItem = ({
   text: string
 }) => {
   return (
-    <MenuItem onClick={handleClose}>
-      <Stack direction={'row'} spacing={'0.38rem'} alignItems={'center'}>
+    <MenuItem dense onClick={handleClose} sx={style.menuItemStyle}>
+      <Stack
+        direction={'row'}
+        spacing={'0.375rem'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        sx={style.menuContentStyle}
+      >
         {icon}
         <Typography variant="Caption" color={'text.alternative'}>
           {text}
@@ -28,13 +36,17 @@ const IconMenuItem = ({
 const DropdownMenu = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
   const open = Boolean(anchorEl)
+  const { isPc } = useMedia()
+
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation()
     setAnchorEl(event.currentTarget)
   }
+
   const handleClose = () => {
     setAnchorEl(null)
   }
+
   return (
     <div>
       <Button
@@ -43,12 +55,21 @@ const DropdownMenu = () => {
         aria-haspopup="true"
         aria-expanded={open ? 'true' : undefined}
         onClick={handleClick}
+        sx={{
+          width: '2.5rem',
+          height: '2.5rem',
+          boxSizing: 'border-box',
+          p: '0.0625rem',
+        }}
       >
         <MoreHorizontalIcon
           sx={{
             color: 'text.alternative',
             width: '1.25rem',
             height: '1.25rem',
+            boxSizing: 'border-box',
+            transform: isPc || anchorEl ? 'rotate(0deg)' : 'rotate(90deg)',
+            transition: 'transform 0.2s ease-in-out',
           }}
         />
       </Button>
@@ -57,30 +78,51 @@ const DropdownMenu = () => {
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
         anchorOrigin={{
-          vertical: 'bottom',
+          vertical: 'top',
           horizontal: 'right',
         }}
         transformOrigin={{
           vertical: 'top',
           horizontal: 'right',
         }}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+          dense: true,
+          sx: style.dropdownMenuStyle,
+        }}
       >
+        <MenuItem
+          onClick={handleClose}
+          sx={{ ...style.menuItemStyle, position: 'relative' }}
+        >
+          <MoreHorizontalIcon
+            sx={{
+              color: 'text.alternative',
+              width: '1.25rem',
+              height: '1.25rem',
+              boxSizing: 'border-box',
+              transform: isPc || anchorEl ? 'rotate(0deg)' : 'rotate(90deg)',
+              transition: 'transform 0.2s ease-in-out',
+              position: 'absolute',
+              right: '0.6rem',
+            }}
+          />
+        </MenuItem>
         <IconMenuItem
           handleClose={handleClose}
           icon={
             <ShareIcon
               sx={{
                 color: 'text.alternative',
-                width: '1.25rem',
-                height: '1.25rem',
+                width: '1.5rem',
+                height: '1.5rem',
+                padding: '0.125rem',
+                boxSizing: 'border-box',
               }}
             />
           }
-          text={'공유하기'}
+          text={'공유'}
         />
         <IconMenuItem
           handleClose={handleClose}
@@ -88,12 +130,12 @@ const DropdownMenu = () => {
             <ReportIcon
               sx={{
                 color: 'text.alternative',
-                width: '1.25rem',
-                height: '1.25rem',
+                width: '1.5rem',
+                height: '1.5rem',
               }}
             />
           }
-          text={'신고하기'}
+          text={'신고'}
         />
       </Menu>
     </div>
