889c8f4d9befa126078cc984c8c3e6aed3c59a94
SaltySalt77 <alexiena@naver.com>
2024-01-04 17:01:24 +0900

    Design: Removed width height from svg icon

--- src/icons/CloseIcon.tsx
+++ src/icons/CloseIcon.tsx
@@ -1,13 +1,7 @@
 import { createSvgIcon } from '@mui/material'
 
 const CloseIcon = createSvgIcon(
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 20 20"
-    fill="none"
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none">
     <path
       d="M15 5L5 15"
       stroke="currentColor"
