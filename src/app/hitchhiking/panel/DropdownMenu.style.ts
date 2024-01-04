fb0c9b7815649f0e916b2d9da63edbecb88b1bf7
SaltySalt77 <alexiena@naver.com>
2024-01-04 23:05:51 +0900

    Design: Changed dropdown design

---
+++ src/app/hitchhiking/panel/DropdownMenu.style.ts
@@ -0,0 +1,22 @@
+import { SxProps } from '@mui/material'
+
+export const menuItemStyle: SxProps = {
+  paddingTop: 0,
+  paddingBottom: 0,
+  height: '2.5rem',
+  width: '7.5rem',
+  boxSizing: 'border-box',
+}
+
+export const menuContentStyle: SxProps = {
+  boxSizing: 'border-box',
+  height: '1.5rem',
+  width: '7.5rem',
+}
+
+export const dropdownMenuStyle: SxProps = {
+  width: '7.5rem',
+  boxSizing: 'border-box',
+  paddingTop: 0,
+  paddingBottom: '1rem',
+}
