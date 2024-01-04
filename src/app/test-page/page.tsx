08873195427915ca4907e00bad35c3cbeaf49dbc
SaltySalt77 <alexiena@naver.com>
2024-01-05 00:31:07 +0900

    Test: Fix 'use client' specification in testing

---
+++ src/app/test-page/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DropdownMenu from '../hitchhiking/panel/DropdownMenu'
+
+const TestPage = () => {
+  return (
+    <div>
+      <DropdownMenu />
+    </div>
+  )
+}
+
+export default TestPage
--- src/icons/ArrowUp.tsx
+++ src/icons/ArrowUp.tsx
@@ -1,24 +1,25 @@
 'use client'
 import { createSvgIcon } from '@mui/material'
+import React from 'react'
 
-const SearchIcon = createSvgIcon(
+const ArrowUp = createSvgIcon(
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none">
     <path
-      d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z"
+      d="M10 15.8359V4.16927"
       stroke="currentColor"
       strokeWidth="1.25"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      d="M17.5 17.5L13.875 13.875"
+      d="M15.8346 10.0039L10.0013 4.17057L4.16797 10.0039"
       stroke="currentColor"
       strokeWidth="1.25"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
   </svg>,
-  'SearchIcon',
+  'ArrowUp',
 )
 
-export default SearchIcon
+export default ArrowUp
