4abc7d425a475f2292e2662aec1ce4a76b94b332
SaltySalt77 <alexiena@naver.com>
2024-01-08 18:16:11 +0900

    Design: Ensure consistent ratio of navbar and content
    Resolved the issue of inconsistent ratios between the navigation bar and page content on subpages of My Page, ensuring a harmonized layout.

--- src/app/my-page/panel/my-page.style.ts
+++ src/app/my-page/panel/my-page.style.ts
@@ -1,17 +1,10 @@
 import { SxProps } from '@mui/material'
 
 const pagePcStyle: SxProps = {
-  // py: ['2rem'],
-  // px: ['1.5rem', '2rem'],
   // maxWidth: '52.75rem',
-  // minWidth: '25rem',
-  // width: '47vw',
+  width: '100%',
 }
 
-const pageMobileStyle: SxProps = {
-  // padding: 0,
-  // maxWidth: '52.75rem',
-  // width: '91vw',
-}
+const pageMobileStyle: SxProps = {}
 
 export { pagePcStyle, pageMobileStyle }
