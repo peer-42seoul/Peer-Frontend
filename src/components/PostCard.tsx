9bfebc3b4fa49885040a59d989f87f9913f18765
SaltySalt77 <alexiena@naver.com>
2024-01-09 18:20:47 +0900

    Refact: Generalize PostCard common component for versatile usage
    Adjusted the code of the PostCard common component to ensure its compatibility and usability on various pages, resolving limitations to Hitchhiking page exclusivity.

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -7,35 +7,40 @@ import {
   Typography,
   alpha,
 } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { IPostCard } from '@/types/IPostCard'
 import { ITag } from '@/types/IPostDetail'
 import { Chip } from '@mui/material'
-import useMedia from '@/hook/useMedia'
 import CuAvatar from '@/components/CuAvatar'
 import * as style from './PostCard.style'
 
-const PostCard = React.forwardRef<HTMLDivElement, IPostCard>(function PostCard(
-  {
-    authorImage,
-    teamName,
-    title,
-    tagList,
-    image,
-    sx,
-    onClick,
-    onMouseUp,
-    onTouchEnd,
-    cardWidth, // width 기준으로 text overflow를 조절하므로 꼭 필요합니다.
-  }: IPostCard,
-  ref,
-) {
-  const { isPc } = useMedia()
+function PostCard({
+  authorImage,
+  teamName,
+  title,
+  tagList,
+  image,
+  sx,
+  onClick,
+  onMouseUp,
+  onTouchEnd,
+}: IPostCard) {
+  const ref = React.useRef<HTMLDivElement>(null)
+  const [currentCardWidth, setCurrentCardWidth] = useState<number>(0)
+
+  useEffect(() => {
+    if (ref.current) {
+      setCurrentCardWidth(ref.current.clientWidth)
+    }
+  }, [ref, ref.current?.clientWidth])
 
   const getLineCount = (originHeight: number, lineHeight: number) => {
-    const lineCount = Math.floor((cardWidth * originHeight) / 328 / lineHeight)
+    const lineCount = Math.floor(
+      (currentCardWidth || 328 * originHeight) / 328 / lineHeight,
+    )
     return lineCount ? lineCount : 1
   }
+
   return (
     <Card
       sx={{
@@ -53,7 +58,10 @@ const PostCard = React.forwardRef<HTMLDivElement, IPostCard>(function PostCard(
         component="img"
         image={image}
         alt="post thumbnail"
-        sx={isPc ? style.cardMediaPcStyle : style.cardMediaMobileStyle}
+        sx={{
+          ...style.cardMediaStyleBase,
+          height: (currentCardWidth * 251) / 328,
+        }}
       />
       <Stack
         sx={{ p: '1rem', pt: '0.75rem' }}
@@ -65,7 +73,11 @@ const PostCard = React.forwardRef<HTMLDivElement, IPostCard>(function PostCard(
             <CuAvatar
               aria-label="profile"
               src={authorImage}
-              sx={style.cardAuthorAvatarStyle}
+              sx={{
+                ...style.cardAuthorAvatarStyleBase,
+                height: (currentCardWidth * 32) / 328,
+                width: (currentCardWidth * 32) / 328,
+              }}
             />
           }
           title={
@@ -122,6 +134,6 @@ const PostCard = React.forwardRef<HTMLDivElement, IPostCard>(function PostCard(
       </Stack>
     </Card>
   )
-})
+}
 
 export default PostCard
