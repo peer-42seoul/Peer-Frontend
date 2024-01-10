d372eedae54d6ce51f2c4d02b442fd610dbd523c
yoouyeon <jyeon.yoon59@gmail.com>
2024-01-10 11:38:39 +0900

    Feat: Add toggleDark function

--- src/components/ToastUIEditor.tsx
+++ src/components/ToastUIEditor.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-import React, { useEffect, useRef } from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
 import '@toast-ui/editor/dist/toastui-editor.css'
 import { IEditorOptions } from '@toast-ui/editor'
-import { Card } from '@mui/material'
-// import { useTheme } from '@mui/material/styles'
+import { Card, useTheme } from '@mui/material'
 import useEditorState from '@/states/useEditorState'
 
 /**
@@ -17,23 +16,22 @@ const ToastEditor = ({
   previewStyle = 'vertical',
   height = '30rem',
 }: IEditorOptions) => {
-  // const themed = useTheme()
+  const themed = useTheme()
   const editorRef = useRef<HTMLDivElement>(null)
+  const toggleDark = useCallback(() => {
+    const editorEl = editorRef.current?.getElementsByClassName(
+      'toastui-editor-defaultUI',
+    )[0]
+
+    if (editorEl) {
+      if (themed.palette.mode === 'dark') {
+        editorEl.classList.add('toastui-editor-dark')
+      } else {
+        editorEl.classList.remove('toastui-editor-dark')
+      }
+    }
+  }, [editorRef, themed.palette.mode])
   const { editor, setEditor, resetEditor } = useEditorState()
-
-  // const toggleDark = () => {
-  //   const editorEl = editorRef.current?.getElementsByClassName(
-  //     'toastui-editor-defaultUI',
-  //   )[0]
-
-  //   if (editorEl) {
-  //     if (themed.palette.mode === 'dark') {
-  //       editorEl.classList.add('toastui-editor-dark')
-  //     } else {
-  //       editorEl.classList.remove('toastui-editor-dark')
-  //     }
-  //   }
-  // }
 
   useEffect(() => {
     if (!editorRef.current) {
@@ -46,6 +44,7 @@ const ToastEditor = ({
       height: height,
       initialValue: initialValue,
     })
+    toggleDark()
     return () => {
       editor?.destroy()
       resetEditor()
