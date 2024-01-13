3e95aa264e7a4f9686cbfe65dbea7a8519dc334b
yoouyeon <jyeon.yoon59@gmail.com>
2024-01-14 00:12:56 +0900

    Style: Add Edit Page Design

--- src/app/teams/[id]/notice/@edit/page.tsx
+++ src/app/teams/[id]/notice/@edit/page.tsx
@@ -4,15 +4,29 @@ import useTeamPageState from '@/states/useTeamPageState'
 import { EditPage, EditBox } from '@/components/board/EditPanel'
 import NoticeEditForm from './panel/NoticeEditForm'
 import NoticeEditButton from './panel/NoticeEditButton'
+import useMedia from '@/hook/useMedia'
 
 const NoticeEdit = ({ params }: { params: { id: string } }) => {
+  const { postId, setNotice } = useTeamPageState()
+  const { isPc } = useMedia()
   const { id } = params
-  const { postId } = useTeamPageState()
+  const handleGoBack = () => {
+    if (postId) {
+      setNotice('DETAIL', postId)
+    } else {
+      setNotice('LIST')
+    }
+  }
   return (
-    <EditPage title={postId ? '공지사항 수정' : '공지사항 작성'}>
+    <EditPage
+      isPc={isPc}
+      type={postId ? 'edit' : 'new'}
+      handleGoBack={handleGoBack}
+      title={postId ? '공지사항 수정' : '공지사항 작성'}
+    >
       <EditBox>
         <NoticeEditForm teamId={id} />
-        <NoticeEditButton />
+        {isPc && <NoticeEditButton />}
       </EditBox>
     </EditPage>
   )
