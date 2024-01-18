1f985ac390a808a4446aaf771eba72b3a0792f37
SaltySalt77 <alexiena@naver.com>
2024-01-19 01:57:17 +0900

    Feat: Implement working demo of My Skills Edit component on profile page
    Introduced a functional demo of the My Skills Edit component on the profile page. Acknowledging the existence of bugs that require fixing for seamless user interaction.

--- src/app/my-page/profile/panel/SkillsEditor.tsx
+++ src/app/my-page/profile/panel/SkillsEditor.tsx
@@ -1,14 +1,18 @@
+import useAxiosWithAuth from '@/api/config'
 import CuModal from '@/components/CuModal'
 import TagChip from '@/components/TagChip'
 import { ITag } from '@/types/IPostDetail'
 import {
   Autocomplete,
   Button,
+  CircularProgress,
   Stack,
   TextField,
   Typography,
 } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+const TIMEOUT = 0.5
 
 const SkillsEditor = ({
   open,
@@ -19,8 +23,51 @@ const SkillsEditor = ({
   mutate: () => void
   closeModal: () => void
 }) => {
-  const [selected, setSelected] = useState([] as ITag[])
-  const [tagList, setTagList] = useState([] as ITag[])
+  const [selected, setSelected] = useState([] as ITag[]) // 선택 된 데이터
+  const [tagList, setTagList] = useState([] as ITag[]) // 검색 된 데이터
+
+  const [timeOut, setTimeOut] = useState(TIMEOUT)
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [text, setText] = useState('')
+
+  const axiosWithAuth = useAxiosWithAuth()
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setTimeOut((prev) => prev - 0.25)
+    }, 250)
+
+    return () => clearInterval(countdown)
+  }, [timeOut])
+
+  useEffect(() => {
+    if (timeOut === 0 && text !== '' && isLoading) {
+      axiosWithAuth
+        .get(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/v1/skill/search?keyword=${text}`,
+        )
+        .then((res) => {
+          setTagList((prev) => prev.concat(res.data))
+          setIsLoading(false)
+        })
+        .catch(() => {
+          setIsLoading(false)
+        })
+    }
+  }, [timeOut])
+
+  const handleTextFiledChange = (e: any) => {
+    setText(e.target.value)
+    if (e.target.value === '') {
+      setIsLoading(false)
+      return
+    } else if (isLoading === false) {
+      setIsLoading(true)
+    }
+    setTimeOut(TIMEOUT)
+  }
 
   const handleInput = (_: any, value: readonly string[]) => {
     const selectedTags = value
@@ -54,14 +101,40 @@ const SkillsEditor = ({
         </Typography>
         <Autocomplete
           multiple
+          sx={{
+            fieldset: {
+              height: '2.5rem',
+            },
+          }}
+          loading={isLoading}
           options={tagList.map((tag) => tag.name)}
           onChange={handleInput}
           renderTags={() => <></>}
           renderInput={(params) => (
             <TextField
               {...params}
+              onChange={handleTextFiledChange}
               size="small"
               placeholder={'프레임워크 또는 개발언어를 입력해주세요.'}
+              sx={{ position: 'relative' }}
+              InputProps={{
+                ...params.InputProps,
+                endAdornment: (
+                  <>
+                    {isLoading ? (
+                      <CircularProgress
+                        color="primary"
+                        size={'1.25rem'}
+                        sx={{
+                          position: 'absolute',
+                          left: ['82.5%', '87.5%'],
+                        }}
+                      />
+                    ) : null}
+                    {params.InputProps.endAdornment}
+                  </>
+                ),
+              }}
             />
           )}
         />
@@ -77,9 +150,13 @@ const SkillsEditor = ({
               key={tag.tagId}
               name={tag.name}
               onDelete={() => {
-                setSelected((prev) =>
-                  prev.filter((tag) => tag.tagId !== tag.tagId),
-                )
+                setSelected((prev) => {
+                  const newTags = prev.filter(
+                    (curTag) => curTag.tagId !== tag.tagId,
+                  )
+                  console.log(newTags)
+                  return newTags
+                })
               }}
               color={tag.color}
             />
