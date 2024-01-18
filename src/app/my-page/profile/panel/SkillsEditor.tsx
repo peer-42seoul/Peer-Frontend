bb8233675b55a8ea568a117d44d09a2dc37a78d1
SaltySalt77 <alexiena@naver.com>
2024-01-19 04:08:11 +0900

    Design: Enhance visual layout of My Skills Modification component
    Introduced design enhancements to the My Skills Modification component, improving its visual appeal and layout.

--- src/app/my-page/profile/panel/SkillsEditor.tsx
+++ src/app/my-page/profile/panel/SkillsEditor.tsx
@@ -1,6 +1,8 @@
 import useAxiosWithAuth from '@/api/config'
 import CuModal from '@/components/CuModal'
+import CuTextModal from '@/components/CuTextModal'
 import TagChip from '@/components/TagChip'
+import useModal from '@/hook/useModal'
 import { ITag } from '@/types/IPostDetail'
 import { getUniqueArray } from '@/utils/getUniqueArray'
 import {
@@ -28,13 +30,18 @@ const SkillsEditor = ({
   const [selected, setSelected] = useState([] as Array<string>) // 선택 된 데이터
   const [tagList, setTagList] = useState([] as ITag[]) // 검색 된 데이터
 
-  const [timeOut, setTimeOut] = useState(TIMEOUT)
+  const [text, setText] = useState('') // 검색 텍스트
 
+  const [timeOut, setTimeOut] = useState(TIMEOUT)
   const [isLoading, setIsLoading] = useState(false)
 
-  const [text, setText] = useState('')
-
   const axiosWithAuth = useAxiosWithAuth()
+
+  const {
+    isOpen,
+    openModal: openAlertModal,
+    closeModal: closeAlertModal,
+  } = useModal()
 
   useEffect(() => {
     const countdown = setInterval(() => {
@@ -74,6 +81,7 @@ const SkillsEditor = ({
   const handleInput = (_: any, value: string[]) => {
     setSelected(value)
   }
+
   return (
     <CuModal
       open={open}
@@ -92,75 +100,122 @@ const SkillsEditor = ({
         onClick: closeModal,
       }}
     >
-      <Stack>
-        <Typography variant={'CaptionEmphasis'} color={'text.strong'}>
-          나의 스킬
-        </Typography>
-        <Autocomplete
-          multiple
-          sx={{
-            fieldset: {
-              height: '2.5rem',
+      <>
+        <Stack direction={'column'} spacing={'1rem'} height={[1, undefined]}>
+          <Typography variant={'CaptionEmphasis'} color={'text.strong'}>
+            나의 스킬
+          </Typography>
+          <Autocomplete
+            multiple
+            sx={{
+              fieldset: {
+                height: '2.5rem',
+              },
+            }}
+            loading={isLoading}
+            value={selected}
+            options={tagList.map((tag) => tag.name)}
+            onChange={handleInput}
+            renderTags={() => <></>}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                onChange={handleTextFiledChange}
+                size="small"
+                placeholder={'프레임워크 또는 개발언어를 입력해주세요.'}
+                sx={{ position: 'relative' }}
+                InputProps={{
+                  ...params.InputProps,
+                  endAdornment: (
+                    <>
+                      {isLoading ? (
+                        <CircularProgress
+                          color="primary"
+                          size={'1.25rem'}
+                          sx={{
+                            position: 'absolute',
+                            left: ['82.5%', '87.5%'],
+                          }}
+                        />
+                      ) : null}
+                      {params.InputProps.endAdornment}
+                    </>
+                  ),
+                }}
+              />
+            )}
+          />
+          <Stack
+            rowGap={['1rem', '0.5rem']}
+            columnGap={['6px', '0.5rem']}
+            direction={'row'}
+            flexWrap={'wrap'}
+            justifyContent={'flex-start'}
+            alignItems={'center'}
+          >
+            <Stack
+              direction={'row'}
+              justifyContent={'space-between'}
+              width={1}
+              alignItems={'center'}
+            >
+              <Typography variant={'CaptionEmphasis'} color={'text.strong'}>
+                선택한 스킬
+              </Typography>
+              <Button
+                variant={'text'}
+                sx={{ height: '1.5rem' }}
+                onClick={openAlertModal}
+              >
+                <Typography
+                  variant={'CaptionEmphasis'}
+                  color={'text.alternative'}
+                  lineHeight={'normal'}
+                >
+                  전체 삭제
+                </Typography>
+              </Button>
+            </Stack>
+            {selected.map((tagName) => {
+              const tag = tagList.find((tag) => tag.name === tagName)
+              if (!tag) return null
+              return (
+                <TagChip
+                  key={tag.tagId}
+                  name={tag.name}
+                  onDelete={() => {
+                    setSelected((prev) => {
+                      const newTags = prev.filter(
+                        (curTag) => curTag !== tag.name,
+                      )
+                      console.log(newTags)
+                      return newTags
+                    })
+                  }}
+                  color={tag.color}
+                />
+              )
+            })}
+          </Stack>
+        </Stack>
+        <CuTextModal
+          open={isOpen}
+          title={'스킬 전체 삭제'}
+          content="스킬을 전체 삭제하시겠습니까?"
+          onClose={closeAlertModal}
+          containedButton={{
+            text: '확인',
+            onClick: () => {
+              setSelected([])
+              closeAlertModal()
             },
           }}
-          loading={isLoading}
-          value={selected}
-          options={tagList.map((tag) => tag.name)}
-          onChange={handleInput}
-          renderTags={() => <></>}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              onChange={handleTextFiledChange}
-              size="small"
-              placeholder={'프레임워크 또는 개발언어를 입력해주세요.'}
-              sx={{ position: 'relative' }}
-              InputProps={{
-                ...params.InputProps,
-                endAdornment: (
-                  <>
-                    {isLoading ? (
-                      <CircularProgress
-                        color="primary"
-                        size={'1.25rem'}
-                        sx={{
-                          position: 'absolute',
-                          left: ['82.5%', '87.5%'],
-                        }}
-                      />
-                    ) : null}
-                    {params.InputProps.endAdornment}
-                  </>
-                ),
-              }}
-            />
-          )}
+          textButton={{
+            text: '취소',
+            onClick: closeAlertModal,
+          }}
         />
-        <Stack direction={'row'} justifyContent={'space-between'}>
-          <Typography variant={'CaptionEmphasis'} color={'text.strong'}>
-            선택한 스킬
-          </Typography>
-          <Button variant={'text'}>전체 삭제</Button>
-        </Stack>
-        {selected.map((tagName) => {
-          const tag = tagList.find((tag) => tag.name === tagName)
-          if (!tag) return null
-          return (
-            <TagChip
-              key={tag.tagId}
-              name={tag.name}
-              onDelete={() => {
-                setSelected((prev) => {
-                  const newTags = prev.filter((curTag) => curTag !== tag.name)
-                  console.log(newTags)
-                  return newTags
-                })
-              }}
-              color={tag.color}
-            />
-          )
-        })}
-      </Stack>
+      </>
     </CuModal>
   )
 }
