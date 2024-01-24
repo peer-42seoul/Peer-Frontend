96c4f30d9408ad1891b50aabc866ba72fa341dcc
Woorim Kim <wrkimlimi@gmail.com>
2024-01-24 13:33:48 +0900

    fix: use layout breakpoint

--- src/app/profile/[id]/layout.tsx
+++ src/app/profile/[id]/layout.tsx
@@ -1,17 +1,13 @@
-'use client'
-
 import React from 'react'
-import useMedia from '@/hook/useMedia'
 import { Container, Stack, Box } from '@mui/material'
 import * as style from '../../my-page/layout.style'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const { isPc } = useMedia()
   return (
     <Container sx={style.container}>
       <Stack
         justifyContent={'center'}
-        direction={isPc ? 'row' : 'column'}
+        direction={['column', 'row']}
         spacing={'2rem'}
         sx={style.stack}
       >
