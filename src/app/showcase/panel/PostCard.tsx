267be11ed534120822f8c962f696c6a064699bd9
HiHoi <ghtjd5087@gmail.com>
2024-01-25 18:53:49 +0900

    feat: showcase mobile content add

--- src/app/showcase/panel/PostCard.tsx
+++ src/app/showcase/panel/PostCard.tsx
@@ -1,30 +1,44 @@
 import {
   Box,
   Card,
+  CardActionArea,
   CardContent,
   CardHeader,
   CardMedia,
+  IconButton,
   Stack,
   Typography,
-  // alpha,
+  alpha,
 } from '@mui/material'
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { IPostCardShowcase } from '@/types/IPostCard'
 import { Chip } from '@mui/material'
 import CuAvatar from '@/components/CuAvatar'
 import * as style from './PostCard.style'
 import { IShowcaseTag } from './types'
+import useAxiosWithAuth from '@/api/config'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ThumbUpIcon from '@mui/icons-material/ThumbUp'
 
 function PostCard({
+  postId,
   authorImage,
   teamName,
   title,
   tagList,
   image,
   sx,
+  like,
+  liked,
+  isFavorite,
+  onClick,
 }: IPostCardShowcase) {
   const ref = React.useRef<HTMLDivElement>(null)
   const [currentCardWidth, setCurrentCardWidth] = useState<number>(0)
+  const [favorite, setFavorite] = useState<boolean>(isFavorite)
+  const [likeNum, setLikeNum] = useState<number>(like)
+  const [isLiked, setIsLiked] = useState<boolean>(liked)
+  const axiosWithAuth = useAxiosWithAuth()
 
   useEffect(() => {
     if (ref.current) {
@@ -39,6 +53,35 @@ function PostCard({
     return lineCount ? lineCount : 1
   }
 
+  const clickFavorite = useCallback(() => {
+    axiosWithAuth
+      .post(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/v1/showcase/favorite/${postId}`,
+      )
+      .then((res) => {
+        if (res.status === 200) {
+          console.log(res)
+          setFavorite(!favorite)
+        }
+      })
+  }, [isFavorite, axiosWithAuth])
+
+  const clickLike = useCallback(() => {
+    axiosWithAuth
+      .post(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/showcase/like/${postId}`)
+      .then((res) => {
+        if (res.status === 200) {
+          console.log(res)
+          if (like < res.data.like) {
+            setIsLiked(true)
+          } else {
+            setIsLiked(false)
+          }
+          setLikeNum(res.data.like)
+        }
+      })
+  }, [like, liked, axiosWithAuth])
+
   return (
     <Card
       sx={{
@@ -48,10 +91,11 @@ function PostCard({
         backfaceVisibility: 'hidden',
       }}
       ref={ref}
+      // onClick={onClick}
     >
       <CardMedia
         component="img"
-        image={image!}
+        image={image ? image : '/icons/52.png'}
         alt="post thumbnail"
         sx={{
           ...style.cardMediaStyleBase,
@@ -80,54 +124,85 @@ function PostCard({
               {teamName}
             </Typography>
           }
+          action={
+            <Stack direction={'row'} spacing={'1rem'} padding={'0.5rem'}>
+              <Stack direction={'row'} spacing={'0.5rem'}>
+                <IconButton
+                  onClick={clickLike}
+                  color={isLiked ? 'primary' : 'inherit'}
+                  size="small"
+                  sx={{
+                    m: 0,
+                    p: 0,
+                  }}
+                >
+                  <ThumbUpIcon />
+                </IconButton>
+                <Typography>{likeNum}</Typography>
+              </Stack>
+
+              <IconButton
+                onClick={clickFavorite}
+                color={favorite ? 'primary' : 'inherit'}
+                size="small"
+                sx={{
+                  m: 0,
+                  p: 0,
+                }}
+              >
+                <FavoriteIcon />
+              </IconButton>
+            </Stack>
+          }
           sx={{ p: 0 }}
         />
-        <Box
-          sx={{
-            height: (currentCardWidth * 190) / 328,
-            boxSizing: 'border-box',
-          }}
-        >
-          <CardContent sx={{ p: 0 }}>
-            <Typography
-              variant="Body1"
-              color="text.normal"
-              sx={{
-                ...style.cardTitleStyleBase,
-                height: getLineCount(46, 22.5) * 22.5,
-                WebkitLineClamp: getLineCount(46, 22.5),
-              }}
-            >
-              {title}
-            </Typography>
-          </CardContent>
-          <CardContent sx={{ p: 0 }}>
-            <Stack
-              gap={1}
-              direction={'row'}
-              justifyContent={'center'}
-              sx={{
-                overflow: 'hidden',
-                height: getLineCount(46, 22.5) * 20 + 8,
-              }}
-            >
-              {tagList?.map(({ name }: IShowcaseTag, idx: number) => {
-                return (
-                  <Chip
-                    label={<Typography variant="Tag">{name}</Typography>}
-                    size="small"
-                    key={idx}
-                    style={{
-                      // backgroundColor: alpha(color, 0.3),
-                      borderRadius: 2,
-                      height: '1.25rem',
-                    }}
-                  />
-                )
-              })}
-            </Stack>
-          </CardContent>
-        </Box>
+        <CardActionArea onClick={onClick}>
+          <Box
+            sx={{
+              height: (currentCardWidth * 190) / 328,
+              boxSizing: 'border-box',
+            }}
+          >
+            <CardContent sx={{ p: 0 }}>
+              <Typography
+                variant="Body1"
+                color="text.normal"
+                sx={{
+                  ...style.cardTitleStyleBase,
+                  height: getLineCount(46, 22.5) * 22.5,
+                  WebkitLineClamp: getLineCount(46, 22.5),
+                }}
+              >
+                {title}
+              </Typography>
+            </CardContent>
+            <CardContent sx={{ p: 0 }}>
+              <Stack
+                gap={1}
+                direction={'row'}
+                sx={{
+                  overflow: 'hidden',
+                  height: getLineCount(46, 22.5) * 20 + 8,
+                }}
+              >
+                {tagList?.map(({ name, color }: IShowcaseTag, idx: number) => {
+                  return (
+                    <Chip
+                      label={<Typography variant="Tag">{name}</Typography>}
+                      size="small"
+                      key={idx}
+                      style={{
+                        backgroundColor: alpha(color, 0.3),
+                        borderRadius: 2,
+                        height: '1.25rem',
+                      }}
+                    />
+                  )
+                })}
+              </Stack>
+            </CardContent>
+          </Box>
+        </CardActionArea>
       </Stack>
     </Card>
   )
