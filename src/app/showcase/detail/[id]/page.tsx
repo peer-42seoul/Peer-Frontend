bad76edd93646f10509de719d403ad7c64a8018a
Devicii <62472550+asroq1@users.noreply.github.com>
2024-01-26 17:09:08 +0900

    style : Add style on Shocase-detail

--- src/app/showcase/detail/[id]/page.tsx
+++ src/app/showcase/detail/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Stack, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import React from 'react'
 import ShowcaseViewer from './panel/ShowcaseViewer'
 import { IShowcaseViewerFields } from '@/types/IShowcaseEdit'
@@ -9,7 +9,6 @@ import useSWR from 'swr'
 import { defaultGetFetcher } from '@/api/fetchers'
 
 const ShowcaseDetailPage = ({ params }: { params: { id: number } }) => {
-  console.log('showcase', params.id)
   const { data, isLoading, error } = useSWR<IShowcaseViewerFields>(
     `${process.env.NEXT_PUBLIC_API_URL}/api/v1/showcase/${params.id}`,
     defaultGetFetcher,
@@ -21,11 +20,7 @@ const ShowcaseDetailPage = ({ params }: { params: { id: number } }) => {
   if (error)
     return <Typography color={'error'}>에러가 발생했습니다.</Typography>
 
-  return (
-    <Stack>
-      <ShowcaseViewer data={data} />
-    </Stack>
-  )
+  return <ShowcaseViewer data={data} />
 }
 
 export default ShowcaseDetailPage
--- src/app/showcase/detail/[id]/panel/ShowcaseViewer.tsx
+++ src/app/showcase/detail/[id]/panel/ShowcaseViewer.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from '@mui/material'
+import { Box, Container, Stack } from '@mui/material'
 import React from 'react'
 import TeamName from '../../../panel/common/TeamName'
 import SkillInput from '../../../panel/common/SkillInput'
@@ -8,24 +8,93 @@ import { IShowcaseViewerFields } from '@/types/IShowcaseEdit'
 import ToastViewer from '@/components/ToastUIViewer'
 import LinksViewer from '../../../panel/common/LinksViewer'
 import '@toast-ui/editor/dist/toastui-editor-viewer.css'
+import * as style from './ShowcaseViewer.style'
+import LabelWithIcon from '@/app/showcase/panel/LabelWithIcon'
+import { FileIcon } from '@/icons'
+import * as Style from '../../../panel/common/SkillInput.style'
+import useMedia from '@/hook/useMedia'
+
 interface IShowcaseViewerProps {
   data: IShowcaseViewerFields // IShowcase 타입을 import 해야 합니다.
 }
-const ShowcaseViewer = ({ data }: IShowcaseViewerProps) => {
+
+const CoverImage = ({ image }: { image: string }) => {
   return (
-    <Stack direction={'column'} spacing={'2.5rem'} sx={{ width: '26rem' }}>
-      ## TODO hoslim이 만든 컴포넌트로교체하기
-      {/* <InteractiveButtonGroup /> */}
-      <Box component="img" src={data?.image} alt="쇼케이스 이미지" />
-      <TeamName teamName={data?.name} />
+    <Box
+      component="img"
+      src={image}
+      alt="쇼케이스 이미지"
+      sx={style.imageViewer}
+    />
+  )
+}
+
+const InformationViewer = ({ isPc, data }: any) => {
+  return (
+    <Stack spacing={'2rem'} sx={style.InformationViewer(isPc)}>
       <StartEndDateViewer start={data?.start} end={data?.end} />
       <SkillInput skills={data?.skills} />
       <TeamMembers members={data?.member} />
       <LinksViewer links={data?.links} />
-      <ToastViewer initialValue={data?.content} height="30rem" />
-      ## TODO 댓글 추가하기
-      {/* <CommentPage /> */}
     </Stack>
+  )
+}
+
+const ContentViewer = ({ content }: { content: string }) => {
+  return (
+    <Stack sx={{ gap: '0.75rem' }}>
+      <LabelWithIcon
+        svgIcon={<FileIcon sx={Style.IconStyle} />}
+        message="내용"
+        color="text.alternative"
+      />
+      <ToastViewer
+        initialValue={content}
+        height="30rem"
+        sx={{ width: '100%', boxSizing: 'border-box' }}
+      />
+    </Stack>
+  )
+}
+
+const ShowcaseViewer = ({ data }: IShowcaseViewerProps) => {
+  const { isPc } = useMedia()
+  return (
+    <>
+      {isPc ? (
+        <Container sx={style.pcViewer}>
+          <Stack>
+            <TeamName teamName={data?.name} />
+            <Stack sx={style.InformationViewerBox}>
+              <InformationViewer isPc={isPc} data={data} />
+              <Stack
+                spacing={'1rem'}
+                display={'flex'}
+                width={'30%'}
+                marginBottom={'1rem'}
+              >
+                <CoverImage image={data.image} />
+              </Stack>
+            </Stack>
+          </Stack>
+          <ContentViewer content={data?.content} />
+        </Container>
+      ) : (
+        <Container sx={style.mobileViewer}>
+          <Stack
+            spacing={'1rem'}
+            display={'flex'}
+            width={'100%'}
+            marginBottom={'1rem'}
+          >
+            <TeamName teamName={data?.name} />
+            <CoverImage image={data.image} />
+          </Stack>
+          <InformationViewer isPc={isPc} data={data} />
+          <ContentViewer content={data?.content} />
+        </Container>
+      )}
+    </>
   )
 }
 
