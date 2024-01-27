fba57ae6da28c4301035fc179d9a856c0591e962
yoouyeon <jyeon.yoon59@gmail.com>
2024-01-28 01:35:23 +0900

    Feat: Change IPreviewModalProps

--- src/app/teams/[id]/panel/widgets/CalenderWidget/CalendarModal.tsx
+++ src/app/teams/[id]/panel/widgets/CalenderWidget/CalendarModal.tsx
@@ -10,19 +10,37 @@ interface IPreviewModalProps {
   open: boolean
   onClose: () => void
   events?: IEvent[]
+  teamId: number
+}
+
+interface IModalData {
+  type: TCalendarModalType
+  teamId?: number
+  eventId?: number
+  widgetId?: string
 }
 
 type TCalendarModalType = 'preview' | 'create'
 
-const CalendarModal = ({ open, onClose, events }: IPreviewModalProps) => {
-  const [modalType, setModalType] = useState<TCalendarModalType>('preview')
-  switch (modalType) {
+const CalendarModal = ({
+  open,
+  onClose,
+  events,
+  teamId,
+}: IPreviewModalProps) => {
+  const [modalData, setModalData] = useState<IModalData>({
+    type: 'preview',
+    teamId,
+  })
+  switch (modalData.type) {
     case 'preview':
       return (
         <CalendarModalContainer open={open} onClose={onClose} title={'캘린더'}>
           <PreviewModalContent
             events={events}
-            openNewModal={() => setModalType('create')}
+            openNewModal={() =>
+              setModalData((prev) => ({ ...prev, type: 'create' }))
+            }
           />
         </CalendarModalContainer>
       )
