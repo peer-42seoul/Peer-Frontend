2c45f5d0132b3a5ea5809b165963666bd8e9ad0c
SaltySalt77 <alexiena@naver.com>
2024-01-28 01:07:16 +0900

    Docs: Renamed filename due to typo

--- src/api/jwtToken.ts
+++ src/api/jwtToken.ts
@@ -1,8 +1,7 @@
 import * as jose from 'jose'
 
 // 서버 컴포넌트에서만 사용합니다.
-export const getToken = async (apiType: any, secret: string) => {
-  const payLoad = { apiType: apiType }
+export const getToken = async (payLoad: any, secret: string) => {
   const secretKey = await new TextEncoder().encode(secret)
   const token = await new jose.SignJWT(payLoad)
     .setProtectedHeader({ alg: 'HS256' })
