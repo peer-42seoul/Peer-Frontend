67582e63d479e730a1f4ccd2db29ea3c18a480df
yoouyeon <jyeon.yoon59@gmail.com>
2024-01-29 16:40:55 +0900

    Feat: Add EventId

--- src/types/WidgetDataTypes.ts
+++ src/types/WidgetDataTypes.ts
@@ -9,8 +9,9 @@ export interface IEvent extends Event {
   // title: React.ReactNode | undefined // extends Event
   // start: Date // extends Event
   // end: Date // extends Event
+  id: string // 위젯에서 사용할 id
+  eventId: number // 일정 알림에서 사용할 id
   teamId: number
   member: IMember[]
-  eventId: number
   memo: string
 }
