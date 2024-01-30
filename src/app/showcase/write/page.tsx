51e15a42b0a460d648b2d0f931dee12ea64d3139
Devicii <62472550+asroq1@users.noreply.github.com>
2024-01-30 17:21:00 +0900

    refactor : Add error respnose messages

--- src/app/showcase/write/page.tsx
+++ src/app/showcase/write/page.tsx
@@ -17,14 +17,19 @@ const ShowCaseWritePage = () => {
 
   const { data, isLoading, error } = useSWR<IShowcaseEditorFields>(
     `${process.env.NEXT_PUBLIC_API_URL}/api/v1/showcase/write/${teamId}`,
-    // defaultGetFetcher,
     (url: string) => axiosWithAuth.get(url).then((res) => res.data),
     { shouldRetryOnError: false },
   )
 
   if (isLoading) return <CuCircularProgress color={'secondary'} />
-  if (error)
+  if (error) {
+    if (error.response && error.response.status === 409) {
+      return (
+        <Typography color={'error'}>{error.response.data.message}</Typography>
+      )
+    }
     return <Typography color={'error'}>에러가 발생했습니다.</Typography>
+  }
 
   return (
     <Stack direction={'column'} sx={{ overflow: 'auto' }}>
