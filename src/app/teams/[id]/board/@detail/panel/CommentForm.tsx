ef58e394da09ddd09fffdcb373f59da357a2b42a
yoouyeon <jyeon.yoon59@gmail.com>
2024-01-30 17:56:58 +0900

    Fix: Post Comment api URL 고침

--- src/app/teams/[id]/board/@detail/panel/CommentForm.tsx
+++ src/app/teams/[id]/board/@detail/panel/CommentForm.tsx
@@ -1,4 +1,5 @@
 import { FormEvent, useState } from 'react'
+import { useSWRConfig } from 'swr'
 import useAxiosWithAuth from '@/api/config'
 import { CommentFormContainer } from '@/components/board/CommentPanel'
 
@@ -10,19 +11,21 @@ interface ICommentFormProps {
 export const CommentForm = ({ postId, teamId }: ICommentFormProps) => {
   const axiosWithAuth = useAxiosWithAuth()
   const [isLoading, setIsLoading] = useState(false)
+  const { mutate } = useSWRConfig()
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     setIsLoading(true)
     const formData = new FormData(e.currentTarget)
     axiosWithAuth
-      .post('/api/v1/team/board/posts/comment', {
+      .post('/api/v1/team/post/comment/', {
         teamId: teamId,
         postId: postId,
         content: formData.get('new-content') as string,
       })
       .then(() => {
         setIsLoading(false)
+        mutate(`/api/v1/team/post/comment/${postId}?page=1&pageSize=100`) // 댓글 데이터 만료
       })
       .catch(() => {
         alert('댓글 작성에 실패했습니다.')
