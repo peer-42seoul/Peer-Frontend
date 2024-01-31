8680cd1db87451344645eb810df2230fef527ea1
SaltySalt77 <alexiena@naver.com>
2024-02-01 00:21:04 +0900

    Refact: Re-designate typography variants as per designer specifications

---
+++ src/app/recruit/write/page.style.ts
@@ -0,0 +1,26 @@
+import { SxProps } from '@mui/material'
+
+export const componentName: SxProps = {
+  alignItems: 'center',
+}
+
+export const interviewButtonMobileStyle: SxProps = {
+  alignItems: 'center',
+  with: '100%',
+}
+
+export const containerStyle: SxProps = {
+  pt: [undefined, '4rem'],
+  maxWidth: '76rem',
+}
+
+export const boxStyle: SxProps = {
+  boxSizing: 'border-box',
+  width: '100%',
+  borderRadius: ['1rem', '0.75rem'],
+  paddingX: '1rem',
+  paddingY: '1.5rem',
+  paddingTop: ['2.5rem', undefined],
+  backgroundColor: 'background.secondary',
+  // overflowY: 'scroll',
+}
--- src/app/recruit/write/page.tsx
+++ src/app/recruit/write/page.tsx
@@ -27,57 +27,10 @@ import TagAutoComplete from '@/components/TagAutoComplete'
 import axios from 'axios'
 import useMedia from '@/hook/useMedia'
 import useAuthStore from '@/states/useAuthStore'
+import * as style from './page.style'
+import LabelWithIcon from '@/components/LabelWithIcon'
 // icons
-import ImageIcon from '@mui/icons-material/Image'
-import ContentPasteOutlinedIcon from '@mui/icons-material/ContentPasteOutlined'
-import EditOutlinedIcon from '@mui/icons-material/EditOutlined'
-import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined'
-import HowToRegOutlinedIcon from '@mui/icons-material/HowToRegOutlined'
-import WifiOutlinedIcon from '@mui/icons-material/WifiOutlined'
-import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined'
-import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined'
-import InsertLinkOutlinedIcon from '@mui/icons-material/InsertLinkOutlined'
-import LocalOfferOutlinedIcon from '@mui/icons-material/LocalOfferOutlined'
-import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined'
-import CreateNewFolderOutlinedIcon from '@mui/icons-material/CreateNewFolderOutlined'
-
-const componentName = {
-  alignItems: 'center',
-}
-
-const interviewButtonMobileStyle = {
-  alignItems: 'center',
-  with: '100%',
-}
-
-const Pc_Container = {
-  paddingTop: '64px',
-}
-
-const Pc_Style = {
-  width: '1216px',
-  border: '1px solid #000',
-  borderRadius: '12px',
-  paddingTop: '24px',
-  paddingBottom: '24px',
-  paddingLeft: '16px',
-  paddingRight: '16px',
-  backgroundColor: '#18182B',
-}
-
-const Mobile_Container = {}
-
-const Mobile_Style = {
-  width: '100%',
-  borderRadius: '12px',
-  paddingTop: '24px',
-  paddingBottom: '24px',
-  paddingLeft: '16px',
-  paddingRight: '16px',
-  overflowY: 'scroll',
-  border: '2px solid #000',
-  backgroundColor: '#18182B',
-}
+import * as Icon from '@/icons'
 
 const CreateTeam = () => {
   const [title, setTitle] = useState<string>('')
@@ -199,239 +152,224 @@ const CreateTeam = () => {
 
   return (
     <>
-      <Container sx={isPc ? Pc_Container : Mobile_Container}>
+      <Container sx={style.containerStyle}>
         {isPc ? (
-          <Box sx={{ paddingBottom: '24px' }}>
-            <Typography fontSize={'13px'}>모집 글 쓰기</Typography>
+          <Box sx={{ paddingBottom: '1.5rem' }}>
+            <Typography variant="CaptionEmphasis" lineHeight={'normal'}>
+              모집 글 쓰기
+            </Typography>
           </Box>
         ) : null}
-        <Container sx={isPc ? Pc_Style : Mobile_Style}>
-          <Stack gap={3}>
-            {/* 대표이미지 */}
-            <Box>
-              <Stack direction={'row'} gap={1} sx={componentName}>
-                <ImageIcon sx={{ color: 'white' }} />
-                <Typography>대표 이미지</Typography>
-              </Stack>
-              <ImageUploadButton
-                setImage={setImage}
-                setPreviewImage={setPreviewImage}
-              >
-                <Box>
-                  <Image
-                    src={previewImage}
-                    width={240}
-                    height={160}
-                    alt="Picture of the author"
-                  />
-                </Box>
-              </ImageUploadButton>
-            </Box>
-            {/* 스터디 or 프로젝트 선택 */}
-            <Box>
-              <Stack direction={'row'} gap={1} sx={componentName}>
-                <ContentPasteOutlinedIcon sx={{ color: 'white' }} />
-                <Typography variant="h6">팀 분류</Typography>
-              </Stack>
-              <RowRadioButtonsGroup setValue={setType} />
-            </Box>
-            {/* 모집글 제목 */}
-            <Box>
-              <Stack direction={'row'} gap={1} sx={componentName}>
-                <EditOutlinedIcon sx={{ color: 'white' }} />
-                <Typography variant="h6">모집글 제목</Typography>
-              </Stack>
-              <TextField
-                sx={isPc ? { width: '26rem' } : { width: '100%' }}
-                variant="outlined"
-                value={title}
-                onChange={(e) => {
-                  if (e.target.value.length > 20)
-                    setTitle(e.target.value.slice(0, 20) as string)
-                  else setTitle(e.target.value as string)
-                }}
-                placeholder="모집글 제목을 입력해주세요."
-              />
-            </Box>
-            {/* 스터디 명 / 프로젝트 명 */}
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              <Stack direction={'row'} gap={1} sx={componentName}>
-                <FormatListBulletedOutlinedIcon sx={{ color: 'white' }} />
-                <Typography variant="h6">
-                  {type === 'STUDY' ? '스터디 명' : '프로젝트 명'}
-                </Typography>
-              </Stack>
-              <TextField
-                sx={isPc ? { width: '26rem' } : { width: '100%' }}
-                variant="outlined"
-                value={name}
-                onChange={(e) => {
-                  if (e.target.value.length > 20)
-                    setName(e.target.value.slice(0, 20) as string)
-                  else setName(e.target.value as string)
-                }}
-                placeholder="스터디 명 / 프로젝트 명을 입력해주세요."
-              />
-            </Box>
-            {/* (프로젝트인 경우만) 역할 추가 */}
-            {type === 'STUDY' ? null : (
-              <Box>
-                <Stack direction={'row'} gap={1} sx={componentName}>
-                  <HowToRegOutlinedIcon sx={{ color: 'white' }} />
-                  <Typography variant="h6">역할</Typography>
-                </Stack>
-                <SetTeamRole roleData={roleList} setRoleData={setRoleList} />
-              </Box>
-            )}
-            {type === 'STUDY' && (
-              <Box>
-                <Stack direction={'row'} gap={1} sx={componentName}>
-                  <HowToRegOutlinedIcon sx={{ color: 'white' }} />
-                  <Typography variant="h6">인원</Typography>
-                </Stack>
-                <BasicSelect
-                  type={ComponentType.TeamSize}
-                  value={teamsize}
-                  setValue={setTeamsize}
-                />
-              </Box>
-            )}
-            {/* 온/오프라인 활동방식 선택 */}
-            <Box>
-              <Stack direction={'row'} gap={1} sx={componentName}>
-                <WifiOutlinedIcon sx={{ color: 'white' }} />
-                <Typography variant="h6">활동방식</Typography>
-              </Stack>
-              <BasicSelect
-                type={ComponentType.Place}
-                value={place}
-                setValue={setPlace}
-              />
-            </Box>
-            {/* 목표기간 */}
-            <Box>
-              <Stack direction={'row'} gap={1} sx={componentName}>
-                <AccessTimeOutlinedIcon sx={{ color: 'white' }} />
-                <Typography variant="h6">목표기간</Typography>
-              </Stack>
-              <BasicSelect
-                type={ComponentType.Month}
-                value={due}
-                setValue={setMonth}
-              />
-            </Box>
-            {/* 지역 선택 */}
-            {place === 'ONLINE' ? null : (
-              <Box>
-                <Stack direction={'row'} gap={1} sx={componentName}>
-                  <LocationOnOutlinedIcon sx={{ color: 'white' }} />
-                  <Typography variant="h6">지역</Typography>
-                </Stack>
-                <SelectRegion setValue={setRegion} />
-              </Box>
-            )}
-            {/* 커뮤니케이션 링크 등록 */}
-            <Box>
-              <Stack direction={'row'} gap={1} sx={componentName}>
-                <InsertLinkOutlinedIcon sx={{ color: 'white' }} />
-                <Typography variant="h6">소통 링크</Typography>
-              </Stack>
-              <SetCommunicationToolLink setValue={setCommunicationTool} />
-            </Box>
-            {/* 태그 추가 */}
-            <Box>
-              <Stack direction={'row'} gap={1} sx={componentName}>
-                <LocalOfferOutlinedIcon sx={{ color: 'white' }} />
-                <Typography variant="h6">태그</Typography>
-              </Stack>
-              {allTagList ? (
-                <TagAutoComplete
-                  tagList={allTagList}
-                  datas={tagList}
-                  setData={setTagList}
-                  style={
-                    isPc
-                      ? { width: '26rem', height: '2rem' }
-                      : { width: '100%', height: '2rem' }
-                  }
-                />
-              ) : null}
-            </Box>
-            {/* 팀 소개 글 작성 (커스텀에디터 적용되어야 할 부분) */}
-            <Box>
-              <Stack direction={'row'} gap={1} sx={componentName}>
-                <DescriptionOutlinedIcon sx={{ color: 'white' }} />
-                <Typography variant="h6">팀 소개</Typography>
-              </Stack>
-              <TextField
-                variant="outlined"
-                value={content}
-                sx={
-                  isPc
-                    ? { width: '1150px', height: 'auto' }
-                    : { width: '100%', height: 'auto' }
-                }
-                onChange={(e) => {
-                  if (e.target.value.length > 1000)
-                    setContent(e.target.value.slice(0, 1000) as string)
-                  else setContent(e.target.value as string)
-                }}
-                multiline
-              />
-            </Box>
-            {/* 모집 인터뷰 */}
-            <Stack>
-              <Stack
-                direction={'row'}
-                gap={1}
-                sx={isPc ? componentName : interviewButtonMobileStyle}
-              >
-                <CreateNewFolderOutlinedIcon sx={{ color: 'white' }} />
-                <Typography
-                  variant="h6"
-                  sx={{ paddingRight: '5px', width: '70%' }}
-                >
-                  모집인원 인터뷰 등록하기
-                </Typography>
-              </Stack>
-              <Button
-                sx={isPc ? { width: '26rem' } : { width: '100%' }}
-                variant="outlined"
-                onClick={() => setOpenBasicModal(true)}
-              >
-                인터뷰 추가
-              </Button>
-              <SetInterview
-                openBasicModal={openBasicModal}
-                handleCloseBasicModal={setOpenBasicModal}
-                interviewData={interviewList}
-                setInterviewData={setInterviewList}
-              />
-            </Stack>
-            {/* 등록, 취소 버튼 */}
-            <Stack
-              direction={'row'}
-              gap={2}
-              sx={isPc ? componentName : interviewButtonMobileStyle}
-              justifyContent={'flex-end'}
+        <Stack spacing={'1.5rem'} sx={style.boxStyle}>
+          {/* 대표이미지 */}
+          <Box>
+            <LabelWithIcon
+              svgIcon={<Icon.ImageIcon sx={{ color: 'text.normal' }} />}
+              message="대표이미지"
+            />
+            <ImageUploadButton
+              setImage={setImage}
+              setPreviewImage={setPreviewImage}
             >
-              <CuButton
-                message="취소"
-                action={() => {
-                  router.push('/')
-                }}
-                variant="outlined"
+              {/* 폴백 이미지 바꾸기 */}
+              <Box>
+                <Image
+                  src={previewImage}
+                  width={240}
+                  height={160}
+                  alt="Picture of the author"
+                />
+              </Box>
+            </ImageUploadButton>
+          </Box>
+          {/* 스터디 or 프로젝트 선택 */}
+          <Box>
+            <LabelWithIcon
+              message="유형"
+              svgIcon={<Icon.ClipboardIcon sx={{ color: 'text.normal' }} />}
+            />
+            <RowRadioButtonsGroup setValue={setType} />
+          </Box>
+          {/* 모집글 제목 */}
+          <Box>
+            <LabelWithIcon
+              message="모집글 제목"
+              svgIcon={<Icon.EditIcon sx={{ color: 'text.normal' }} />}
+            />
+            <TextField
+              sx={isPc ? { width: '26rem' } : { width: '100%' }}
+              variant="outlined"
+              value={title}
+              onChange={(e) => {
+                if (e.target.value.length > 20)
+                  setTitle(e.target.value.slice(0, 20) as string)
+                else setTitle(e.target.value as string)
+              }}
+              placeholder="모집글 제목을 입력해주세요."
+            />
+          </Box>
+          {/* 스터디 명 / 프로젝트 명 */}
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <LabelWithIcon
+              message={type === 'STUDY' ? '스터디 명' : '프로젝트 명'}
+              svgIcon={<Icon.ListIcon sx={{ color: 'text.normal' }} />}
+            />
+            <TextField
+              sx={isPc ? { width: '26rem' } : { width: '100%' }}
+              variant="outlined"
+              value={name}
+              onChange={(e) => {
+                if (e.target.value.length > 20)
+                  setName(e.target.value.slice(0, 20) as string)
+                else setName(e.target.value as string)
+              }}
+              placeholder="스터디 명 / 프로젝트 명을 입력해주세요."
+            />
+          </Box>
+          {/* (프로젝트인 경우만) 역할 추가 */}
+          {type === 'STUDY' ? null : (
+            <Box>
+              <LabelWithIcon
+                message="역할"
+                svgIcon={<Icon.UserCheckIcon sx={{ color: 'text.normal' }} />}
               />
-              <CuButton
-                message="등록하기"
-                action={onHandlerFinish}
-                variant="contained"
+              <SetTeamRole roleData={roleList} setRoleData={setRoleList} />
+            </Box>
+          )}
+          {type === 'STUDY' && (
+            <Box>
+              <LabelWithIcon
+                message="모집인원"
+                svgIcon={<Icon.UserCheckIcon sx={{ color: 'text.normal' }} />}
               />
-            </Stack>
+              <BasicSelect
+                type={ComponentType.TeamSize}
+                value={teamsize}
+                setValue={setTeamsize}
+              />
+            </Box>
+          )}
+          {/* 온/오프라인 활동방식 선택 */}
+          <Box>
+            <LabelWithIcon
+              message="활동방식"
+              svgIcon={<Icon.WifiIcon sx={{ color: 'text.normal' }} />}
+            />
+            <BasicSelect
+              type={ComponentType.Place}
+              value={place}
+              setValue={setPlace}
+            />
+          </Box>
+          {/* 목표기간 */}
+          <Box>
+            <LabelWithIcon
+              message="목표기간"
+              svgIcon={<Icon.PieChartIcon sx={{ color: 'text.normal' }} />}
+            />
+            <BasicSelect
+              type={ComponentType.Month}
+              value={due}
+              setValue={setMonth}
+            />
+          </Box>
+          {/* 지역 선택 */}
+          {place === 'ONLINE' ? null : (
+            <Box>
+              <LabelWithIcon
+                message="지역"
+                svgIcon={<Icon.LocationIcon sx={{ color: 'text.normal' }} />}
+              />
+              <SelectRegion setValue={setRegion} />
+            </Box>
+          )}
+          {/* 커뮤니케이션 링크 등록 */}
+          <Box>
+            <LabelWithIcon
+              message="커뮤니케이션 링크"
+              svgIcon={<Icon.LinkDiagonalIcon sx={{ color: 'text.normal' }} />}
+            />
+            <SetCommunicationToolLink setValue={setCommunicationTool} />
+          </Box>
+          {/* 태그 추가 */}
+          <Box>
+            <LabelWithIcon
+              message="기술 스택"
+              svgIcon={<Icon.TagIcon sx={{ color: 'text.normal' }} />}
+            />
+            {allTagList ? (
+              <TagAutoComplete
+                tagList={allTagList}
+                datas={tagList}
+                setData={setTagList}
+                style={
+                  isPc
+                    ? { width: '26rem', height: '2rem' }
+                    : { width: '100%', height: '2rem' }
+                }
+              />
+            ) : null}
+          </Box>
+          {/* 팀 소개 글 작성 (커스텀에디터 적용되어야 할 부분) */}
+          <Box>
+            <LabelWithIcon
+              message="팀 소개 글"
+              svgIcon={<Icon.FileIcon sx={{ color: 'text.normal' }} />}
+            />
+            <TextField
+              variant="outlined"
+              value={content}
+              onChange={(e) => {
+                if (e.target.value.length > 1000)
+                  setContent(e.target.value.slice(0, 1000) as string)
+                else setContent(e.target.value as string)
+              }}
+              fullWidth
+              multiline
+            />
+          </Box>
+          {/* 모집 인터뷰 */}
+          <Stack>
+            <LabelWithIcon
+              message="모집 인터뷰"
+              svgIcon={<Icon.FolderPlusIcon sx={{ color: 'text.normal' }} />}
+            />
+            <Button
+              sx={isPc ? { width: '26rem' } : { width: '100%' }}
+              variant="outlined"
+              onClick={() => setOpenBasicModal(true)}
+            >
+              인터뷰 추가
+            </Button>
+            <SetInterview
+              openBasicModal={openBasicModal}
+              handleCloseBasicModal={setOpenBasicModal}
+              interviewData={interviewList}
+              setInterviewData={setInterviewList}
+            />
           </Stack>
-        </Container>
+          {/* 등록, 취소 버튼 */}
+          <Stack
+            direction={'row'}
+            gap={2}
+            sx={isPc ? style.componentName : style.interviewButtonMobileStyle}
+            justifyContent={'flex-end'}
+          >
+            <CuButton
+              message="취소"
+              action={() => {
+                router.push('/')
+              }}
+              variant="outlined"
+            />
+            <CuButton
+              message="등록하기"
+              action={onHandlerFinish}
+              variant="contained"
+            />
+          </Stack>
+        </Stack>
       </Container>
-      <Box sx={{ height: '70px' }} />
     </>
   )
 }
