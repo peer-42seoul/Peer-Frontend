e22af86e078e0f162069aaf1ac71eccc87e4415e
HiHoi <ghtjd5087@gmail.com>
2024-02-03 14:58:53 +0900

    feat: contact us add

--- src/app/about/@announce/page.tsx
+++ src/app/about/@announce/page.tsx
@@ -70,7 +70,7 @@ const AnnounceCard = ({ title, writer, date, id }: AnnounceCardProps) => {
 
 const AnnouncePage = () => {
   const { isPc } = useMedia()
-  const [page, setPage] = useState<number>(0)
+  const [page, setPage] = useState<number>(1)
   const { data, isLoading, error } = useSWR<IPagination<AnnounceCardProps[]>>(
     `${process.env.NEXT_PUBLIC_API_URL}/api/v1/about/announcement?page=${page}&size=5`,
     defaultGetFetcher,
