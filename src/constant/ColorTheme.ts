1b5394c03af900fb0b2a10b93e49f8bd233e6392
SaltySalt77 <alexiena@naver.com>
2024-02-04 16:06:59 +0900

    Design: Add fonts

--- src/constant/ColorTheme.ts
+++ src/constant/ColorTheme.ts
@@ -1,6 +1,18 @@
 import { EDisplayMode } from '@/types/DisplayTypes'
 import { alpha, createTheme } from '@mui/material'
 
+const pretendardVariable = {
+  fontFamily: 'Pretendard Variable',
+  fontStyle: 'normal',
+  fontDisplay: 'swap',
+  fontWeight: 400,
+  src: `
+    url("/fonts/PretendardVariable") format('woff2')
+  `,
+  unicodeRange:
+    'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
+}
+
 const customBreakpoints = {
   xs: 0,
   sm: 480,
@@ -42,6 +54,14 @@ export const darkTheme = createTheme({
       mobileNavTab: '#0F0F27', // 모바일 네비게이션 탭 배경색
     },
   },
+  typography: {
+    fontFamily: 'Pretendard Variable, sans-serif',
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: pretendardVariable,
+    },
+  },
 })
 
 export const lightTheme = createTheme({
@@ -75,6 +95,14 @@ export const lightTheme = createTheme({
     custom: {
       main: '#F7F8FA',
       mobileNavTab: '#F7F8FA', // 모바일 네비게이션 탭 배경색. light 관련 사항은 여기 피그마 코멘트 참고
+    },
+  },
+  typography: {
+    fontFamily: 'Pretendard Variable, sans-serif',
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: pretendardVariable,
     },
   },
 })
