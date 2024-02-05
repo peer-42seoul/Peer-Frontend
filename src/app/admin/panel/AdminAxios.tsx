5868172298d62433f4973120396f7e096d1736d3
jujeon <jwjsjwnsstjd@gmail.com>
2024-02-05 23:41:41 +0900

    test

--- src/app/admin/panel/AdminAxios.tsx
+++ src/app/admin/panel/AdminAxios.tsx
@@ -1,16 +1,23 @@
-import axios from 'axios';
+import axios from 'axios'
+
+export const config = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+}
 
 export const fetchTags = async () => {
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const API_URL = process.env.NEXT_PUBLIC_API_URL
   try {
     const response = await axios.get(`${API_URL}/api/v1/tag`, {
       withCredentials: true,
       // peer-test 도메인에서만 httpOnly sameSite 쿠키를 전달받을 수 있으므로 로컬에서 테스트 할 동안 임시로 주석처리
-    });
-    console.log(response);
-    return response.data;
+    })
+    console.log(response)
+    return response.data
   } catch (error) {
-    console.error('태그를 불러오는 데 실패했습니다.', error);
-    throw error;
+    console.error('태그를 불러오는 데 실패했습니다.', error)
+    throw error
   }
-};
+}
