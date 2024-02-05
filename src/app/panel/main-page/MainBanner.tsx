3ceaf1cd44ac2e3a5ea9aa7e4ee86578e6fbdeeb
HiHoi <ghtjd5087@gmail.com>
2024-02-05 20:57:55 +0900

    fix: change banner and carusel

--- src/app/panel/main-page/MainBanner.tsx
+++ src/app/panel/main-page/MainBanner.tsx
@@ -1,8 +1,10 @@
 import { Box } from '@mui/material'
 import useMedia from '@/hook/useMedia'
+import { useRouter } from 'next/navigation'
 
 const MainBanner = () => {
   const { isPc } = useMedia()
+  const router = useRouter()
   // const settings = {
   //   dots: false,
   //   infinite: true,
@@ -20,12 +22,14 @@ const MainBanner = () => {
       <img
         src={
           !isPc
-            ? '/images/banners/default-mobile.svg'
-            : '/images/banners/default-pc.svg'
+            ? '/images/banners/about-mobile.svg'
+            : '/images/banners/about-pc.svg'
         }
         alt={'main-banner'}
         width={'100%'}
         height={'100%'}
+        style={{ cursor: 'pointer' }}
+        onClick={() => router.push('/about')}
       />
     </Box>
   )
--- src/app/panel/main-page/MainCarousel.tsx
+++ src/app/panel/main-page/MainCarousel.tsx
@@ -3,10 +3,8 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
 
 const MainCarousel = () => {
-  const router = useRouter()
   const settings = {
     dots: true,
     infinite: true,
@@ -23,22 +21,12 @@ const MainCarousel = () => {
     height: '100%',
   }
 
-  // const imageProps = {
-  //   width: 310,
-  //   height: 130,
-  //   style: {
-  //     borderRadius: '0.75rem',
-  //     height: '7.5rem',
-  //   },
-  // }
-
-  const cursorImageProps = {
+  const imageProps = {
     width: 310,
     height: 130,
     style: {
       borderRadius: '0.75rem',
       height: '7.5rem',
-      cursor: 'pointer',
     },
   }
 
@@ -46,10 +34,9 @@ const MainCarousel = () => {
     <Slider {...settings}>
       <Box sx={BoxStyle}>
         <Image
-          src={'/images/banners/about-mobile.svg'}
+          src={'/images/banners/default-mobile.svg'}
           alt="banner-1"
-          onClick={() => router.push('/about')}
-          {...cursorImageProps}
+          {...imageProps}
         />
       </Box>
     </Slider>
