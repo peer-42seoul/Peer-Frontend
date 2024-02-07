ce16a34b20dbb666be4e621233a8a8c6281345db
Hyeon Ji Kim <guswly801@gmail.com>
2024-02-07 19:23:03 +0900

    fix showcase page

--- src/app/showcase/panel/common/StartEndDateViewer.style.ts
+++ src/app/showcase/panel/common/StartEndDateViewer.style.ts
@@ -1,9 +1,10 @@
 export const StartEndDateViewerBox = (isPc?: boolean) => ({
-  display: isPc ? 'flex' : 'none',
-  justifyContent: isPc ? 'space-between' : 'none',
+  display: 'flex',
+  justifyContent: 'space-between',
   alignItems: 'center',
   gap: '1rem',
   gridArea: 'startEndDateViewer',
   width: '100%',
   height: isPc ? '6.25rem' : '3.375rem',
+  marginTop: isPc ? '0' : '1.25rem',
 })
