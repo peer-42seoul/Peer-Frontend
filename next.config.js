ecab5d4f287a8c820d97c240daceb55c9d8b9762
HiHoi <ghtjd5087@gmail.com>
2024-02-14 17:26:43 +0900

    fix: pwa setting

--- next.config.js
+++ next.config.js
@@ -5,8 +5,7 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: false,
-  // disable: prod ? false : true, // 나중에 true로 바꿔야함
+  disable: prod ? false : true, // 나중에 true로 바꿔야함
   // runtimeCaching,
 })
 
