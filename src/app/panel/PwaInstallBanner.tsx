6a0def86e31deb9bbfc0e6156ff7e01195258bd6
Woorim Kim <wrkimlimi@gmail.com>
2024-02-14 14:16:55 +0900

    fix: change position fixed into sticky

--- src/app/panel/PwaInstallBanner.tsx
+++ src/app/panel/PwaInstallBanner.tsx
@@ -81,10 +81,14 @@ const PwaInstallBanner = () => {
       <>
         {isShowInstall && (
           <Box
-            position={'fixed'}
-            bottom={0}
+            position={'sticky'}
+            bottom={isPc ? '0' : '76px'}
             width={'100%'}
-            sx={{ backgroundColor: 'primary.main', zIndex: 9999 }}
+            sx={{
+              backgroundColor: 'primary.main',
+              zIndex: 9999,
+              paddingBottom: 0.2,
+            }}
           >
             <Stack
               margin={1}
@@ -115,10 +119,14 @@ const PwaInstallBanner = () => {
     <>
       {isShowInstall && (
         <Box
-          position={'fixed'}
-          bottom={0}
+          position={'sticky'}
+          bottom={isPc ? '0' : '76px'}
           width={'100%'}
-          sx={{ backgroundColor: 'primary.main', zIndex: 9999 }}
+          sx={{
+            backgroundColor: 'primary.main',
+            zIndex: 9999,
+            paddingBottom: 0.2,
+          }}
         >
           <Stack margin={1}>
             <Typography color={'white'} variant="Caption">
