586c01bc7080387cfefa670cbdaf04b783e8e856
Devicii <62472550+asroq1@users.noreply.github.com>
2024-03-05 19:59:27 +0900

    fix: getIcon has changed as a component

--- src/app/recruit/[id]/panel/LinkButton.tsx
+++ src/app/recruit/[id]/panel/LinkButton.tsx
@@ -2,7 +2,7 @@
 import KakaoIcon from '../../../../../public/icons/public/kakaoIcon.svg'
 import GoogleIcon from '../../../../../public/icons/public/googleIcon.svg'
 import SlackIcon from '../../../../../public/icons/public/slackIcon.svg'
-import InstagramIcon from '../../../../../public/icons/public/instagramIcon.svg'
+import DiscordIcon from '../../../../../public/icons/public/discordIcon.svg'
 
 import React from 'react'
 import {
@@ -16,11 +16,55 @@ import {
 import InsertLinkOutlinedIcon from '@mui/icons-material/InsertLinkOutlined'
 import * as style from './LinkButton.style'
 
+interface IIconProps {
+  link: string
+  variant: 'text' | 'outlined' | 'contained'
+}
+
 const LinkTooltip = styled(({ className, ...props }: TooltipProps) => (
   <Tooltip {...props} classes={{ popper: className }} />
 ))(() => ({
   [`& .${tooltipClasses.tooltip}`]: style.tooltip,
 }))
+
+const ResponsiveIcon = ({ link, variant }: IIconProps) => {
+  const convertLink = (link: string) => {
+    const httpPattern = /^https?:\/\//i
+    if (!httpPattern.test(link)) {
+      return `http://${link}`
+    }
+    return link
+  }
+
+  // 추가할 아이콘이 더 생기면 조건문을 더 추가하고, icon/public 폴더에 아이콘을 추가해주세요.
+  const iconHnadler = (link: string) => {
+    if (link.includes('kakao')) {
+      return KakaoIcon
+    } else if (link.includes('google')) {
+      return GoogleIcon
+    } else if (link.includes('slack')) {
+      return SlackIcon
+    } else if (link.includes('discord')) {
+      return DiscordIcon
+    } else {
+      return InsertLinkOutlinedIcon
+    }
+  }
+  const Icon = iconHnadler(link)
+
+  return (
+    <Button
+      variant={variant}
+      href={convertLink(link)}
+      sx={style.button}
+      disabled={!link}
+    >
+      <SvgIcon>
+        <Icon />
+      </SvgIcon>
+    </Button>
+  )
+}
 
 const LinkButton = ({
   href,
@@ -29,87 +73,11 @@ const LinkButton = ({
   href: string
   variant: 'text' | 'outlined' | 'contained'
 }) => {
-  const getIcon = (link: string) => {
-    convertLink(link)
-
-    if (link.includes('kakao')) {
-      return (
-        <Button
-          variant={variant}
-          href={convertLink(href)}
-          sx={style.button}
-          style={{ padding: 0 }}
-          disabled={!href}
-        >
-          <SvgIcon>
-            <KakaoIcon />
-          </SvgIcon>
-        </Button>
-      )
-    } else if (link.includes('google')) {
-      return (
-        <Button
-          variant={variant}
-          href={convertLink(href)}
-          sx={style.button}
-          disabled={!href}
-        >
-          <SvgIcon>
-            <GoogleIcon />
-          </SvgIcon>
-        </Button>
-      )
-    } else if (link.includes('slack')) {
-      return (
-        <Button
-          variant={variant}
-          href={convertLink(href)}
-          sx={style.button}
-          disabled={!href}
-        >
-          <SvgIcon>
-            <SlackIcon />
-          </SvgIcon>
-        </Button>
-      )
-    } else if (link.includes('instagram')) {
-      return (
-        <Button
-          variant={variant}
-          href={convertLink(href)}
-          sx={style.button}
-          disabled={!href}
-        >
-          <SvgIcon>
-            <InstagramIcon />
-          </SvgIcon>
-        </Button>
-      )
-    } else {
-      return (
-        <Button
-          variant={variant}
-          href={convertLink(href)}
-          sx={style.button}
-          disabled={!href}
-        >
-          <InsertLinkOutlinedIcon />
-        </Button>
-      )
-    }
-  }
-
-  const convertLink = (link: string) => {
-    const httpPattern = /^https?:\/\//i
-    if (!httpPattern.test(link)) {
-      return `http://${link}`
-    }
-    return link
-  }
   //@todo 이동불가능한 url 처리
   return (
     <LinkTooltip placement="bottom-start" title={href}>
-      {getIcon(href)}
+      {/* 모집글 링크버튼 */}
+      <ResponsiveIcon link={href} variant={variant} />
     </LinkTooltip>
   )
 }
