4c4b8f662480d51c94ad2292764a37d269da125c
SaltySalt77 <alexiena@naver.com>
2024-03-07 17:45:32 +0900

    Feat: Add AllIcons component for storybook

---
+++ src/stories/AllIcons.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import AllIcons from '@/icons/AllIcons'
+
+const meta: Meta<typeof AllIcons> = {
+  component: AllIcons,
+}
+export default meta
+
+type Story = StoryObj<typeof AllIcons>
+
+export const Default: Story = {}
