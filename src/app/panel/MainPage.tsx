9100a5d125612abf2c8dc7019f654f03cc0c5b2d
Hyeon Ji Kim <94117828+hyeon81@users.noreply.github.com>
2024-03-20 18:35:24 +0900

    fix conflict

--- src/app/panel/MainPage.tsx
+++ src/app/panel/MainPage.tsx
@@ -7,20 +7,6 @@ import MainMobileView from '@/app/panel/main-page/MainMobileView'
 import MainPcView from '@/app/panel/main-page/MainPcView'
 import Footer from '@/app/panel/main-page/Footer'
 import PushAlertBanner from './PushAlertBanner'
-import MainBanner from '@/app/panel/main-page/MainBanner'
-import Tutorial from '@/components/Tutorial'
-import { MainPageTutorial } from '@/components/tutorialContent/MainPageTutorial'
-import NoDataDolphin from '@/components/NoDataDolphin'
-import {
-  cardStyle,
-  containerStyle,
-  floatButtonStyle,
-  sideMenuStyle,
-} from '@/app/panel/main-page/Mainpage.style'
-import SearchOptionPanel from '@/app/panel/main-page/MainPanel'
-import SelectSort from '@/app/panel/main-page/SelectSort'
-import useMedia from '@/hook/useMedia'
-import Footer from './main-page/Footer'
 
 export interface BeforeInstallPromptEvent extends Event {
   readonly platforms: string[]
